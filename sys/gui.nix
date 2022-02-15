799856d797f02ae6e9a7a2b4ce100629fb4564b4
xieby1 <xieby1@outlook.com>
2022-02-15 18:25:19 +0800

    删除无用配置

--- sys/gui.nix
+++ sys/gui.nix
@@ -7,7 +7,6 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.nvidiaWayland = false;
   services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
