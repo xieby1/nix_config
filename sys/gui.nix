e6cd70b1580160d216635332a6f131bba5d46678
xieby1 <xieby1@outlook.com>
2022-02-16 21:02:05 +0800

    删除steam

--- sys/gui.nix
+++ sys/gui.nix
@@ -4,12 +4,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
-
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
@@ -39,5 +37,4 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
-  programs.steam.enable = true;
 }
