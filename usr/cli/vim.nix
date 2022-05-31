6629d2b8b8c488c7e242ec11af4525e006f896d7
xieby1 <xieby1@outlook.com>
2022-05-31 15:39:28 +0800

    [22.05] Fix coc-nvim
    https://github.com/nix-community/home-manager/issues/2966

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -80,7 +80,6 @@ in
       git-messenger-vim
       DrawIt
       vim-nix
-      coc-nvim
       vim-floaterm
       vim-markdown-toc
       vista-vim
@@ -107,6 +106,21 @@ in
     # coc
     withNodeJs = true;
     coc.enable = true;
+    # bug: neovim: rebuilding with coc support does not work when nodejs is in PATH
+    # https://github.com/nix-community/home-manager/issues/2966
+    # Solution:
+    # https://github.com/sumnerevans/home-manager-config/commit/da138d4ff3d04cddb37b0ba23f61edfb5bf7b85e
+    coc.package = pkgs.vimUtils.buildVimPluginFrom2Nix {
+      pname = "coc.nvim";
+      version = "2022-05-21";
+      src = pkgs.fetchFromGitHub {
+        owner = "neoclide";
+        repo = "coc.nvim";
+        rev = "791c9f673b882768486450e73d8bda10e391401d";
+        sha256 = "sha256-MobgwhFQ1Ld7pFknsurSFAsN5v+vGbEFojTAYD/kI9c=";
+      };
+      meta.homepage = "https://github.com/neoclide/coc.nvim/";
+    };
     coc.settings = {
       "suggest.noselect" = true;
       "suggest.enablePreview" = true;
