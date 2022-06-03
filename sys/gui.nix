6c7d2d81e61ed1c2c8b40c2503360417e7dc197e
xieby1 <xieby1@outlook.com>
2022-06-03 14:00:05 +0800

    [22.05] set sound in /etc/nixos/configuration.nix

--- sys/gui.nix
+++ sys/gui.nix
@@ -23,10 +23,6 @@
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
 
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
