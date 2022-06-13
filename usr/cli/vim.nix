9f44b78de6c481a90474e12e4e6dc615500184c2
xieby1 <xieby1@outlook.com>
2022-06-13 15:13:48 +0800

    vim: config local for every buffer

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -62,11 +62,16 @@ let
         sha256 = "1wg6g4rqpj12sjj0g1qxqgcpkzr7x82lk90lf6qczim97r3lj9hy";
       };
     };
-    type = "lua";
     config = ''
+      lua << EOF
       require('config-local').setup {
         lookup_parents = true,
+        silent = true,
       }
+      EOF
+      augroup config-local
+        autocmd BufEnter * nested lua require'config-local'.source()
+      augroup END
     '';
   };
 in
