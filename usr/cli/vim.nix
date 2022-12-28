b6596318eaeb080cbe09ccef6ad51be5b99c75d5
xieby1 <xieby1@outlook.com>
2022-12-28 14:12:27 +0800

    usr: cli: vim: move all vim config to vim.nix

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -10,14 +10,22 @@ let
       sha256 = "1rabyhayxswwh85lp4rzi2w1x1zbp5j0v025vsknzbqi0lqy32nk";
     };
   };
-  vim-mark = pkgs.vimUtils.buildVimPlugin {
-    name = "vim-mark";
-    src = pkgs.fetchFromGitHub {
-      owner = "inkarkat";
-      repo = "vim-mark";
-      rev = "7f90d80d0d7a0b3696f4dfa0f8639bba4906d037";
-      sha256 = "0n8r0ks58ixqv7y1afliabiqwi55nxsslwls7hni4583w1v1bbly";
+  my-vim-mark = {
+    plugin = pkgs.vimUtils.buildVimPlugin {
+      name = "vim-mark";
+      src = pkgs.fetchFromGitHub {
+        owner = "inkarkat";
+        repo = "vim-mark";
+        rev = "7f90d80d0d7a0b3696f4dfa0f8639bba4906d037";
+        sha256 = "0n8r0ks58ixqv7y1afliabiqwi55nxsslwls7hni4583w1v1bbly";
+      };
     };
+    config = ''
+      " clear highlight created by vim-mark
+      nnoremap <leader><F3> :MarkClear<CR>
+      " show all marks
+      nnoremap <leader>M :Marks<CR>
+    '';
   };
   DrawIt = pkgs.vimUtils.buildVimPlugin {
     name = "DrawIt";
@@ -102,27 +110,243 @@ let
     };
     preInstall = "cd vim";
   };
+  my-vim-gitgutter = {
+    plugin = pkgs.vimPlugins.vim-gitgutter;
+    config = ''
+      let g:gitgutter_signs = 1
+      let g:gitgutter_highlight_lines = 1
+      let g:gitgutter_sign_added = '+'
+      let g:gitgutter_sign_modified = '~'
+      let g:gitgutter_sign_removed = '-'
+      nnoremap <Leader>g :GitGutterLineHighlightsToggle<CR>:GitGutterSignsToggle<CR>
+    '';
+  };
+  my-git-messenger-vim = {
+    plugin = pkgs.vimPlugins.git-messenger-vim;
+    config = ''
+      " popup window no close automatically
+      let g:git_messenger_close_on_cursor_moved=v:false
+    '';
+  };
+  my-vim-floaterm = {
+    plugin = pkgs.vimPlugins.vim-floaterm;
+    config = ''
+      nmap <Leader>t :FloatermNew --cwd=<buffer><CR>
+      " let g:floaterm_keymap_new = '<Leader>t'
+      let g:floaterm_width = 0.8
+      let g:floaterm_height = 0.8
+    '';
+  };
+  my-vista-vim = {
+    plugin = pkgs.vimPlugins.vista-vim;
+    config = ''
+      let g:vista_update_on_text_changed = 1
+      let g:vista_default_executive = 'coc'
+    '';
+  };
+  my-vim-markdown = {
+    plugin = pkgs.vimPlugins.vim-markdown;
+    config = ''
+      let g:vim_markdown_new_list_item_indent = 2
+      let g:vim_markdown_no_default_key_mappings = 1
+    '';
+  };
+  my-vim-hexokinase = {
+    plugin = pkgs.vimPlugins.vim-hexokinase;
+    config = ''
+      let g:Hexokinase_highlighters = ['backgroundfull']
+      nnoremap <leader>c :set termguicolors!<CR>
+    '';
+  };
 in
-
 {
   # neovim
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
-    extraConfig = builtins.readFile ./xvimrc;
+    extraConfig = ''
+      " vim
+      "" Highlight searches
+      set hlsearch
+      nnoremap <F3> :nohlsearch<CR>
+      "" Show line number
+      set number
+      "" indent
+      "set smartindent " not good, indentation in empty line cannot be auto removed
+      """ show existing tab with 4 spaces width
+      set tabstop=4
+      """ when indenting with '>', use 4 spaces width
+      set shiftwidth=4
+      """ specicial indent
+      au FileType markdown setlocal shiftwidth=2 tabstop=2
+      """ On pressing tab, insert 4 spaces
+      set expandtab
+      """ line wrap with ident
+      set breakindent
+      """" horizontally scroll 4 characters
+      nnoremap z<left> 4zh
+      nnoremap z<right> 4zl
+      "" tags support, ';' means upward search, refering to http://vimdoc.sourceforge.net/htmldoc/editing.html#file-searching
+      set tags=./tags;
+      "" Fold
+      """ fold text to be the first and last line
+      """ refer to sbernheim4's reply at
+      """ https://github.com/nvim-treesitter/nvim-treesitter/pull/390
+      function! GetSpaces(foldLevel)
+          if &expandtab == 1
+              " Indenting with spaces
+              let str = repeat(" ", a:foldLevel / (&shiftwidth + 1) - 1)
+              return str
+          elseif &expandtab == 0
+              " Indenting with tabs
+              return repeat(" ", indent(v:foldstart) - (indent(v:foldstart) / &shiftwidth))
+          endif
+      endfunction
+      function! MyFoldText()
+          let startLineText = getline(v:foldstart)
+          let endLineText = trim(getline(v:foldend))
+          let indentation = GetSpaces(foldlevel("."))
+          let spaces = repeat(" ", 200)
+          let str = indentation . startLineText . " …… " . endLineText . spaces
+          return str
+      endfunction
+      "" toggle foldmethod between manual and indent
+      set foldmethod=indent
+      :function ToggleFoldmethod()
+      : if (&foldmethod == "manual")
+      :   set foldmethod=indent
+      : else
+      :   set foldmethod=manual
+      : endif
+      :endfunction
+      nnoremap <Leader>z :call ToggleFoldmethod()<CR>:echo &foldmethod<CR>
+      "" Custom display for text when folding
+      set foldtext=MyFoldText()
+      """ Set the foldlevel to a high setting,
+      """ files are always loaded with opened folds.
+      set foldlevel=20
+      """ mouse support " select by pressing shift key!
+      set mouse=a
+      """ matchit.vim " :h matchit-install
+      packadd! matchit
+      "" Preview
+      nnoremap <leader>[ :pc<CR>
+      "" highlight unwanted whitespace
+      set list
+      set listchars=tab:>-,trail:-
+      "" syntax
+      syntax on
+      "" backspace
+      set backspace=indent,eol,start
+      "" wrap line
+      """ https://stackoverflow.com/questions/248102/is-there-any-command-to-toggle-enable-auto-text-wrapping
+      :function ToggleWrap()
+      : if (&wrap == 1)
+      :   set nowrap
+      : else
+      :   set wrap
+      : endif
+      :endfunction
+      nnoremap <F9> :call ToggleWrap()<CR>
+      set updatetime=400
+      "" highlight current line
+      set cursorlineopt=number
+      augroup CursorLine
+        au!
+        au VimEnter,WinEnter,BufWinEnter * setlocal cursorline
+        au WinLeave * setlocal nocursorline
+      augroup END
+      "" clear tailing space
+      nnoremap <leader>f :%s/\s*$//g<CR>
+      
+      " filetype
+      augroup filetype
+          " detect LLVM IR file
+          au! BufRead,BufNewFile *.ll     set filetype=llvm
+          " cpp " from gem5
+          au! BufRead,BufNewFile *.hh.inc,*.cc.inc set filetype=cpp
+      augroup END
+      
+      " cscope
+      " inspired by https://linux-kernel-labs.github.io/refs/heads/master/labs/introduction.html
+      if has("cscope")
+          " Look for a 'cscope.out' file starting from the current directory,
+          " going up to the root directory.
+          let s:dirs = split(getcwd(), "/")
+          while s:dirs != []
+              let s:path = "/" . join(s:dirs, "/")
+              if (filereadable(s:path . "/cscope.out"))
+                  execute "cs add " . s:path . "/cscope.out " . s:path . " -v"
+                  break
+              endif
+              let s:dirs = s:dirs[:-2]
+          endwhile
+      
+          set csto=0  " Use cscope first, then ctags
+          set cst     " Only search cscope
+          set csverb  " Make cs verbose
+      
+          " 0 symbol
+          nmap <C-\>s :cs find s <C-R><C-W><CR>
+          nmap <C-\>S :cs find s<Space>
+          " 1 definition
+          nmap <C-\>g :cs find g <C-R><C-W><CR>
+          nmap <C-\>G :cs find g<Space>
+          " 2 called func
+          nmap <C-\>d :cs find d <C-R><C-W><CR>
+          nmap <C-\>D :cs find d<Space>
+          " 3 calling func
+          nmap <C-\>c :cs find c <C-R><C-W><CR>
+          nmap <C-\>C :cs find c<Space>
+          " 4 text string
+          nmap <C-\>t :cs find t <C-R><C-W><CR>
+          nmap <C-\>T :cs find t<Space>
+          " 6 egrep pattern
+          nmap <C-\>e :cs find e <C-R><C-W><CR>
+          nmap <C-\>E :cs find e<Space>
+          " 7 file
+          nmap <C-\>F :cs find f<Space>
+          " 8 including file
+          nmap <C-\>i :cs find i ^<C-R><C-F>$<CR>
+          nmap <C-\>I :cs find i<Space>
+          " 9 assign
+          nmap <C-\>a :cs find a <C-R><C-W><CR>
+          nmap <C-\>A :cs find a<Space>
+      
+          " Open a quickfix window for the following queries.
+          "set cscopequickfix=s-,c-,d-,i-,t-,e-,g-
+          "nmap <F6> :cnext <CR>
+          "nmap <F5> :cprev <CR>
+      endif
+      
+      " set terminal title
+      "" https://stackoverflow.com/questions/15123477/tmux-tabs-with-name-of-file-open-in-vim
+      autocmd BufEnter * let &titlestring = "" . expand("%:t")
+      set title
+      
+      nnoremap <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
+      \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
+      \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
+      
+      " highlight
+      augroup HiglightTODO
+          autocmd!
+          autocmd WinEnter,VimEnter * :silent! call matchadd('Todo', 'TODO', -1)
+      augroup END
+    '';
     plugins = with pkgs.vimPlugins; [
-      vim-gitgutter
+      my-vim-gitgutter
       vim-smoothie
-      vim-mark
+      my-vim-mark
       vim-ingo-library
       vim-fugitive
-      git-messenger-vim
+      my-git-messenger-vim
       DrawIt
       vim-nix
-      vim-floaterm
+      my-vim-floaterm
       vim-markdown-toc
-      vista-vim
+      my-vista-vim
       vim-commentary
       myTreesitters
       # python lsp support:
@@ -135,9 +359,9 @@ in
       coc-pyright
       # javscript lsp support
       coc-tsserver
-      vim-markdown # format table
+      my-vim-markdown # format table
       tabular
-      vim-hexokinase
+      my-vim-hexokinase
       vim-plugin-AnsiEsc
       my-nvim-config-local
       my-leap-nvim
@@ -188,6 +412,168 @@ in
         };
       };
     };
+    coc.pluginConfig = ''
+      " May need for Vim (not Neovim) since coc.nvim calculates byte offset by count
+      " utf-8 byte sequence
+      set encoding=utf-8
+      " Some servers have issues with backup files, see #649
+      set nobackup
+      set nowritebackup
+
+      " Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
+      " delays and poor user experience
+      set updatetime=300
+
+      " Always show the signcolumn, otherwise it would shift the text each time
+      " diagnostics appear/become resolved
+      set signcolumn=number
+
+      " Use tab for trigger completion with characters ahead and navigate
+      " NOTE: There's always complete item selected by default, you may want to enable
+      " no select by `"suggest.noselect": true` in your configuration file
+      " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+      " other plugin before putting this into your config
+      inoremap <silent><expr> <TAB>
+            \ coc#pum#visible() ? coc#pum#next(1) :
+            \ CheckBackspace() ? "\<Tab>" :
+            \ coc#refresh()
+      inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
+
+      " Make <CR> to accept selected completion item or notify coc.nvim to format
+      " <C-g>u breaks current undo, please make your own choice
+      inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                                    \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
+      function! CheckBackspace() abort
+        let col = col('.') - 1
+        return !col || getline('.')[col - 1]  =~# '\s'
+      endfunction
+
+      " Use <c-space> to trigger completion
+      if has('nvim')
+        inoremap <silent><expr> <c-space> coc#refresh()
+      else
+        inoremap <silent><expr> <c-@> coc#refresh()
+      endif
+
+      " Use `[g` and `]g` to navigate diagnostics
+      " Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
+      nmap <silent> [g <Plug>(coc-diagnostic-prev)
+      nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+      " GoTo code navigation
+      nmap <silent> gd <Plug>(coc-definition)
+      nmap <silent> gy <Plug>(coc-type-definition)
+      nmap <silent> gi <Plug>(coc-implementation)
+      nmap <silent> gr <Plug>(coc-references)
+
+      " Use K to show documentation in preview window
+      nnoremap <silent> K :call ShowDocumentation()<CR>
+
+      function! ShowDocumentation()
+        if CocAction('hasProvider', 'hover')
+          call CocActionAsync('doHover')
+        else
+          call feedkeys('K', 'in')
+        endif
+      endfunction
+
+      " Highlight the symbol and its references when holding the cursor
+      " autocmd CursorHold * silent call CocActionAsync('highlight')
+
+      " Symbol renaming
+      nmap <leader>rn <Plug>(coc-rename)
+
+      " Formatting selected code
+      " xmap <leader>f  <Plug>(coc-format-selected)
+      " nmap <leader>f  <Plug>(coc-format-selected)
+
+      augroup mygroup
+        autocmd!
+        " Setup formatexpr specified filetype(s)
+        autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+        " Update signature help on jump placeholder
+        autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+      augroup end
+
+      " Applying code actions to the selected code block
+      " Example: `<leader>aap` for current paragraph
+      xmap <leader>a  <Plug>(coc-codeaction-selected)
+      nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+      " Remap keys for applying code actions at the cursor position
+      nmap <leader>ac  <Plug>(coc-codeaction-cursor)
+      " Remap keys for apply code actions affect whole buffer
+      nmap <leader>as  <Plug>(coc-codeaction-source)
+      " Apply the most preferred quickfix action to fix diagnostic on the current line
+      nmap <leader>qf  <Plug>(coc-fix-current)
+
+      " Remap keys for applying refactor code actions
+      " nmap <silent> <leader>re <Plug>(coc-codeaction-refactor)
+      " xmap <silent> <leader>r  <Plug>(coc-codeaction-refactor-selected)
+      " nmap <silent> <leader>r  <Plug>(coc-codeaction-refactor-selected)
+
+      " Run the Code Lens action on the current line
+      nmap <leader>cl  <Plug>(coc-codelens-action)
+
+      " Map function and class text objects
+      " NOTE: Requires 'textDocument.documentSymbol' support from the language server
+      xmap if <Plug>(coc-funcobj-i)
+      omap if <Plug>(coc-funcobj-i)
+      xmap af <Plug>(coc-funcobj-a)
+      omap af <Plug>(coc-funcobj-a)
+      xmap ic <Plug>(coc-classobj-i)
+      omap ic <Plug>(coc-classobj-i)
+      xmap ac <Plug>(coc-classobj-a)
+      omap ac <Plug>(coc-classobj-a)
+
+      " Remap <C-f> and <C-b> to scroll float windows/popups
+      if has('nvim-0.4.0') || has('patch-8.2.0750')
+        nnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+        nnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+        inoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(1)\<cr>" : "\<Right>"
+        inoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? "\<c-r>=coc#float#scroll(0)\<cr>" : "\<Left>"
+        vnoremap <silent><nowait><expr> <C-f> coc#float#has_scroll() ? coc#float#scroll(1) : "\<C-f>"
+        vnoremap <silent><nowait><expr> <C-b> coc#float#has_scroll() ? coc#float#scroll(0) : "\<C-b>"
+      endif
+
+      " Use CTRL-S for selections ranges
+      " Requires 'textDocument/selectionRange' support of language server
+      nmap <silent> <C-s> <Plug>(coc-range-select)
+      xmap <silent> <C-s> <Plug>(coc-range-select)
+
+      " Add `:Format` command to format current buffer
+      command! -nargs=0 Format :call CocActionAsync('format')
+
+      " Add `:Fold` command to fold current buffer
+      command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+
+      " Add `:OR` command for organize imports of the current buffer
+      command! -nargs=0 OR   :call     CocActionAsync('runCommand', 'editor.action.organizeImport')
+
+      " Add (Neo)Vim's native statusline support
+      " NOTE: Please see `:h coc-status` for integrations with external plugins that
+      " provide custom statusline: lightline.vim, vim-airline
+      "set statusline^=%{coc#status()}%{get(b:,'coc_current_function',''')}
+
+      " Mappings for CoCList
+      " Show all diagnostics
+      nnoremap <silent><nowait> <space>a  :<C-u>CocList diagnostics<cr>
+      " Manage extensions
+      nnoremap <silent><nowait> <space>e  :<C-u>CocList extensions<cr>
+      " Show commands
+      nnoremap <silent><nowait> <space>c  :<C-u>CocList commands<cr>
+      " Find symbol of current document
+      nnoremap <silent><nowait> <space>o  :<C-u>CocList outline<cr>
+      " Search workspace symbols
+      nnoremap <silent><nowait> <space>s  :<C-u>CocList -I symbols<cr>
+      " Do default action for next item
+      nnoremap <silent><nowait> <space>j  :<C-u>CocNext<CR>
+      " Do default action for previous item
+      nnoremap <silent><nowait> <space>k  :<C-u>CocPrev<CR>
+      " Resume latest coc list
+      nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
+    '';
   };
 
   # 1.
