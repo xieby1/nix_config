723cab61b7b2f22521daab2022358ccdd6d965c0
xieby1 <xieby1@outlook.com>
2023-01-15 14:40:07 +0800

    clustering: usr: cli: gdbinit

--- usr/cli.nix
+++ usr/cli.nix
@@ -328,7 +328,22 @@ in
     target = ".gdbinit";
   };
   home.file.gdb_dashboard_init = {
-    source = ./cli/gdbinit;
+    text = ''
+      # gdb-dashboard init file
+
+      # available layout modules
+      #   stack registers history assembly
+      #   breakpoints expressions memory
+      #   source threads variables
+      dashboard -layout source
+
+      # https://en.wikipedia.org/wiki/ANSI_escape_code
+      #dashboard -style prompt
+      ## fg bold blue
+      dashboard -style prompt_not_running "\\[\\e[1;34m\\]$\\[\\e[0m\\]"
+      ## fg bold green
+      dashboard -style prompt_running "\\[\\e[1;32m\\]$\\[\\e[0m\\]"
+    '';
     target = ".gdbinit.d/init";
   };
 
