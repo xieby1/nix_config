62a61a14299f5fa150ca3930cfacc5dcc99f4ef7
xieby1 <xieby1@outlook.com>
2023-01-17 00:06:01 +0800

    usr: cli: vim: explicitly set floaterm bg black

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -139,6 +139,8 @@ let
       " let g:floaterm_keymap_new = '<Leader>t'
       let g:floaterm_width = 0.8
       let g:floaterm_height = 0.8
+      " Set floaterm window's background
+      hi Floaterm guibg=black
     '';
   };
   my-vista-vim = {
