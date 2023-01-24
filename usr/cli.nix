7dd620508bcf8a13679630654b1ade4fcd492014
xieby1 <xieby1@outlook.com>
2023-01-24 22:21:16 +0800

    usr: cli: add fzf-doc

--- usr/cli.nix
+++ usr/cli.nix
@@ -19,6 +19,41 @@ let
       chmod a+x $dst
     '';
   };
+  fzf-doc = pkgs.writeScriptBin "fzf-doc" ''
+    allCmds() {
+      # bash alias
+      compgen -A alias
+
+      # external commands
+      # https://unix.stackexchange.com/questions/94775/list-all-commands-that-a-shell-knows
+      case "$PATH" in
+        (*[!:]:) PATH="$PATH:" ;;
+      esac
+      set -f
+      IFS_OLD="$IFS"
+      IFS=:
+      for dir in $PATH; do
+        set +f
+        [ -z "$dir" ] && dir="."
+        for file in "$dir"/*; do
+          if [ -x "$file" ] && ! [ -d "$file" ]; then
+            echo "''${file##*/}"
+          fi
+        done
+      done
+      IFS="$IFS_OLD"
+    }
+
+    cd ~/Documents
+    FILE=$(fzf)
+    [ -z "$FILE" ] && exit
+
+    CMD=$(allCmds | fzf)
+    [ -z "$CMD" ] && exit
+
+    # FILE name may contain space, quote FILE name
+    eval "$CMD" \"$FILE\"
+  '';
   sysconfig = (
     if (builtins.tryEval <nixos-config>).success
     then import <nixpkgs/nixos> {}
@@ -31,7 +66,10 @@ in
     ./cli/tcl.nix
     ./cli/network.nix
   ] ++ [{ # functions & attrs
-    home.packages = [pkgs.fzf];
+    home.packages = [
+      pkgs.fzf
+      fzf-doc
+    ];
     programs.bash.bashrcExtra = ''
       # FZF top-down display
       export FZF_DEFAULT_OPTS="--reverse"
