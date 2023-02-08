0f26fccd773b731e5ed8a3a4620e5b792be3b427
xieby1 <xieby1@outlook.com>
2023-02-08 21:40:52 +0800

    usr: cli: vim: replace gitgutter with gitsigns

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -129,17 +129,6 @@ let
     };
     preInstall = "cd vim";
   };
-  my-vim-gitgutter = {
-    plugin = pkgs.vimPlugins.vim-gitgutter;
-    config = ''
-      let g:gitgutter_signs = 1
-      let g:gitgutter_highlight_lines = 1
-      let g:gitgutter_sign_added = '+'
-      let g:gitgutter_sign_modified = '~'
-      let g:gitgutter_sign_removed = '-'
-      nnoremap <Leader>g :GitGutterLineHighlightsToggle<CR>:GitGutterSignsToggle<CR>
-    '';
-  };
   my-git-messenger-vim = {
     plugin = pkgs.vimPlugins.git-messenger-vim;
     config = ''
@@ -183,7 +172,51 @@ let
     type = "lua";
     config = ''
       require('gitsigns').setup {
+        signcolumn = false,
+        numhl      = true,
+        linehl     = true,
+
         current_line_blame = true,
+
+        -- keymaps
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          -- Actions
+          map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+          map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+          map('n', '<leader>hS', gs.stage_buffer)
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hR', gs.reset_buffer)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+          map('n', '<leader>hD', function() gs.diffthis('~') end)
+          map('n', '<leader>td', gs.toggle_deleted)
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
       }
     '';
   };
@@ -398,7 +431,6 @@ in
       cnoremap <expr> <Right> pumvisible() ? "\<Down>"  : "\<Right>"
     '';
     plugins = with pkgs.vimPlugins; [
-      my-vim-gitgutter
       vim-smoothie
       my-vim-mark
       vim-ingo-library
