79ac83378b7206add6ac096023598040befb3f08
xieby1 <xieby1@outlook.com>
2023-02-11 19:13:12 +0800

    usr: cli: vim: higher contrast

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -226,6 +226,17 @@ let
       nnoremap <leader>c :set termguicolors!<CR>
       set termguicolors
 
+      let g:sonokai_colors_override = {
+      \ 'black':    ['#111215', '237'],
+      \ 'bg0':      ['#22232a', '235'],
+      \ 'bg1':      ['#33353f', '236'],
+      \ 'bg2':      ['#444754', '236'],
+      \ 'bg3':      ['#555869', '237'],
+      \ 'bg4':      ['#666a7e', '237'],
+      \ 'grey':     ['#a5a5a6', '246'],
+      \ 'grey_dim': ['#787879', '240'],
+      \}
+
       " custom sonokai,
       " see section "How to use custom colors" of `:h sonokai.vim`
       function! s:sonokai_custom() abort
