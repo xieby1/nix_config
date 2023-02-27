d915cc94ef8033d5318b17d0c119b59c13bf8a2b
xieby1 <xieby1@outlook.com>
2023-02-27 10:39:49 +0800

    sys: cli: add binfmt for aarch64/riscv

--- sys/cli.nix
+++ sys/cli.nix
@@ -58,4 +58,17 @@
   networking.extraHosts = if builtins.pathExists /home/xieby1/Gist/Config/hosts
   then builtins.readFile /home/xieby1/Gist/Config/hosts
   else "";
+
+  boot.binfmt.registrations = {
+    aarch64-linux = {
+      interpreter = "${pkgs.qemu}/bin/qemu-aarch64";
+      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7\x00'';
+      mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\x00\xff\xfe\xff\xff\xff'';
+    };
+    riscv64-linux = {
+      interpreter = "${pkgs.qemu}/bin/qemu-riscv64";
+      magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xf3\x00'';
+      mask = ''\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+    };
+  };
 }
