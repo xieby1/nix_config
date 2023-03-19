fd3a545cd6404ca3e3196498d498d08a1577bf04
xieby1 <xieby1@outlook.com>
2023-03-19 16:14:10 +0800

    add unstable pkgs

--- config.nix
+++ config.nix
@@ -3,14 +3,10 @@
   allowUnsupportedSystem = true;
   allowUnfree = true;
   packageOverrides = pkgs: {
-    nur = import (pkgs.fetchFromGitHub {
-      # https://github.com//NUR/commit/
-      owner = "nix-community";
-      repo = "NUR";
-      rev = "d67c890fc1096b9c8706c1c46f8e3fec06add355";
-      sha256 = "1afx34940l6yf4n7dgljdyk0r8gbn7p26kkk9n9wqq4m8ng1bmh2";
-    }) {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
+    # pkgs unstable
+    pkgsu = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
   };
 }
