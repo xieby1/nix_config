388f08f27de82d5ff09115a10fcc9634445fc23a
xieby1 <xieby1@outlook.com>
2023-11-10 15:11:16 +0800

    usr: cli: add act

--- usr/cli.nix
+++ usr/cli.nix
@@ -235,6 +235,31 @@ in
         ${pandora}/bin/pandora -t ~/Gist/Pandora-ChatGPT/access_token.dat $@
       '';
     in [pandora chatgpt];
+  }{
+    home.packages = [
+      pkgs.act
+      (pkgs.writeShellScriptBin "act-podman" ''
+        CMD=(
+          "${pkgs.act}/bin/act"
+          "--bind"
+
+          # use podman
+          "--container-daemon-socket" "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
+
+          # use host proxy
+          "--container-options" "--network=host"
+          "--env" "HTTPS_PROXY='http://127.0.0.1:${toString config.proxyPort}'"
+          "--env" "HTTP_PROXY='http://127.0.0.1:${toString config.proxyPort}'"
+          "--env" "FTP_PROXY='http://127.0.0.1:${toString config.proxyPort}'"
+          "--env" "https_proxy='http://127.0.0.1:${toString config.proxyPort}'"
+          "--env" "http_proxy='http://127.0.0.1:${toString config.proxyPort}'"
+          "--env" "ftp_proxy='http://127.0.0.1:${toString config.proxyPort}'"
+
+          "$@"
+        )
+        eval "''${CMD[@]}"
+      '')
+    ];
   }];
 
   home.packages = with pkgs; [
