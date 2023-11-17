30be91383ef3cfb906ff71c92f4bc19ef2f1c1ab
xieby1 <xieby1@outlook.com>
2023-11-17 20:33:04 +0800

    usr: cli: vim: add auto completion

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -338,6 +338,67 @@ let
         },
       }
       -- end of ccls
+      -- Add additional capabilities supported by nvim-cmp
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+      local servers = { 'ltex', 'ccls' }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup {
+          -- on_attach = my_custom_on_attach,
+          capabilities = capabilities,
+        }
+      end
+    '';
+  };
+  my-nvim-cmp = {
+    plugin = pkgs.vimPlugins.nvim-cmp;
+    type = "lua";
+    config = ''
+      local cmp = require'cmp'
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+          ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+          -- C-b (back) C-f (forward) for snippet placeholder navigation.
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({{
+          name = 'nvim_lsp',
+        }}, {{
+          name = 'buffer',
+        }}, {{
+          name = 'path',
+        }}, {{
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' }
+          }
+        }})
+      })
     '';
   };
   my-hbac = {
@@ -604,7 +665,14 @@ in
       git-wip
       my-gitsigns-nvim
       my-color-scheme
+
       my-nvim-lspconfig
+      my-nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-path
+      cmp-cmdline
+
       my-hbac
     ] ++ (lib.optional config.isGui markdown-preview-nvim);
     vimdiffAlias = true;
