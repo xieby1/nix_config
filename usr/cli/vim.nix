380dd7d2256291dedbb8eef74deab89f96f2c2be
xieby1 <xieby1@outlook.com>
2023-11-17 22:47:25 +0800

    usr: cli: vim: remove luasnip config

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -381,8 +381,6 @@ let
           ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
             else
               fallback()
             end
@@ -390,8 +388,6 @@ let
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
