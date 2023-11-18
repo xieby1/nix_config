6102d13db860ec42431516d898bdc3e4767a2082
xieby1 <xieby1@outlook.com>
2023-11-18 13:29:52 +0800

    usr: cli: vim: auto complete words from all visible buffers

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -397,6 +397,16 @@ let
           name = 'nvim_lsp',
         }}, {{
           name = 'buffer',
+          option = {
+            -- completion using words from visible buffers
+            get_bufnrs = function()
+              local bufs = {}
+              for _, win in ipairs(vim.api.nvim_list_wins()) do
+                bufs[vim.api.nvim_win_get_buf(win)] = true
+              end
+              return vim.tbl_keys(bufs)
+            end
+          },
         }}, {{
           name = 'path',
         }}, {{
