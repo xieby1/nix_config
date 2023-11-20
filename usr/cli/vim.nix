7947569c6bfa27829a7e482faf5a00c5dde5747e
xieby1 <xieby1@outlook.com>
2023-11-20 09:35:20 +0800

    usr: cli: vim: group metals settings

--- usr/cli/vim.nix
+++ usr/cli/vim.nix
@@ -463,44 +463,69 @@ let
       })
     '';
   };
-  jre_with_proxy = pkgs.callPackage ./jre_with_proxy.nix {
-    jre = pkgs.openjdk8_headless;
-    proxyHost = "127.0.0.1";
-    proxyPort = "${toString config.proxyPort}";
-  };
-  my-nvim-metals = {
-    plugin = pkgs.vimPlugins.nvim-metals;
-    type = "lua";
-    config = ''
-      -- lspconfig.metals.setup{}
-      local metals_config = require("metals").bare_config()
-      metals_config.settings = {
-        showImplicitArguments = true,
-        excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-        serverProperties = {
-          "-Dhttps.proxyHost=127.0.0.1",
-          "-Dhttps.proxyPort=${toString config.proxyPort}",
-          "-Dhttp.proxyHost=127.0.0.1",
-          "-Dhttp.proxyPort=${toString config.proxyPort}",
-        },
-        -- see `:h metalsBinaryPath`, "Another setting for you crazy Nix kids." Hahaha!
-        metalsBinaryPath = "${pkgs.metals.override {jre = jre_with_proxy;}}/bin/metals",
-        javaHome = "${jre_with_proxy}",
-      }
-      metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- Autocmd that will actually be in charging of starting the whole thing
-      local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-      vim.api.nvim_create_autocmd("FileType", {
-        group = nvim_metals_group,
-        pattern = { "scala", "sbt" },
-        callback = function()
-          require("metals").initialize_or_attach(metals_config)
-        end,
-      })
-    '';
-  };
 in
 {
+  imports = [(let
+    jre_with_proxy = pkgs.callPackage ./jre_with_proxy.nix {
+      jre = pkgs.openjdk8_headless;
+      proxyHost = "127.0.0.1";
+      proxyPort = "${toString config.proxyPort}";
+    };
+    my-nvim-metals = {
+      plugin = pkgs.vimPlugins.nvim-metals;
+      type = "lua";
+      config = ''
+        -- lspconfig.metals.setup{}
+        local metals_config = require("metals").bare_config()
+        metals_config.settings = {
+          showImplicitArguments = true,
+          excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+          serverProperties = {
+            "-Dhttps.proxyHost=127.0.0.1",
+            "-Dhttps.proxyPort=${toString config.proxyPort}",
+            "-Dhttp.proxyHost=127.0.0.1",
+            "-Dhttp.proxyPort=${toString config.proxyPort}",
+          },
+          -- see `:h metalsBinaryPath`, "Another setting for you crazy Nix kids." Hahaha!
+          metalsBinaryPath = "${pkgs.metals.override {jre = jre_with_proxy;}}/bin/metals",
+          javaHome = "${jre_with_proxy}",
+        }
+        metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+        -- Autocmd that will actually be in charging of starting the whole thing
+        local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+          group = nvim_metals_group,
+          pattern = { "scala", "sbt" },
+          callback = function()
+            require("metals").initialize_or_attach(metals_config)
+          end,
+        })
+      '';
+    };
+  in {
+    programs.neovim = {
+      plugins = with pkgs.vimPlugins; [
+        my-nvim-metals
+      ];
+      extraPackages = with pkgs; [
+        (coursier.override {
+          jre = jre_with_proxy;
+        })
+      ];
+
+    };
+    # nvim-metals proxy for bloop
+    home.file.jvmopts = {
+      text = ''
+        -Dhttps.proxyHost=127.0.0.1
+        -Dhttps.proxyPort=${toString config.proxyPort}
+        -Dhttp.proxyHost=127.0.0.1
+        -Dhttp.proxyPort=${toString config.proxyPort}
+      '';
+      target = ".bloop/.jvmopts";
+    };
+  })];
+
   # neovim
   programs.bash.shellAliases.view = "nvim -R";
   # nvim as manpager
@@ -746,7 +771,6 @@ in
       my-cmp-tabnine
 
       my-hbac
-      my-nvim-metals
     ] ++ (lib.optional config.isGui markdown-preview-nvim);
     vimdiffAlias = true;
     extraPackages = with pkgs; [
@@ -754,10 +778,6 @@ in
       ltex-ls
       ripgrep
       pkgsu.nixd
-      # nvim-metals
-      (coursier.override {
-        jre = jre_with_proxy;
-      })
     ];
 
     # coc
@@ -944,16 +964,5 @@ in
       " Resume latest coc list
       nnoremap <silent><nowait> <space>p  :<C-u>CocListResume<CR>
     '';
-  };
-
-  # nvim-metals proxy for bloop
-  home.file.jvmopts = {
-    text = ''
-      -Dhttps.proxyHost=127.0.0.1
-      -Dhttps.proxyPort=${toString config.proxyPort}
-      -Dhttp.proxyHost=127.0.0.1
-      -Dhttp.proxyPort=${toString config.proxyPort}
-    '';
-    target = ".bloop/.jvmopts";
   };
 }
