4c1d54e39196a2cbe1336a65339be7e307f835c6
xieby1 <xieby1@outlook.com>
2023-11-24 20:06:39 +0800

    markcode: config.nix

--- config.nix
+++ config.nix
@@ -1,13 +1,26 @@
-# see pkgs/top-level/config.nix
+#MC # config.nix
+#MC
+#MC When nixpkgs is initialized,
+#MC it will read `~/.config/nixpkgs/config.nix` as the parameter of nixpkgs.config.
+#MC If interested in initialization,
+#MC go to read the source code of nixpkgs `<nixpkgs>/pkgs/top-level/impure.nix`.
+#MC The possible parameters are listed in official nixpkgs manual
+#MC [config Options Reference](https://nixos.org/manual/nixpkgs/stable/#sec-config-options-reference),
+#MC or go to read the source code `<nixpkgs>/pkgs/top-level/config.nix`.
+#MC
+#MC My `config.nix` is annotated and listed as follows:
 {
-  # disable install non-native packages, like install x86_64-linux packages on aarch64-linux
+  #MC Disable installing non-native packages,
+  #MC like preventing x86_64-linux packages installed on aarch64-linux.
   allowUnsupportedSystem = false;
   allowUnfree = true;
   packageOverrides = pkgs: {
+    #MC Add nix user repository (NUR) to nixpkgs.
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
-    # pkgs unstable
+    #MC Add unstable nixpkgs to nixpkgs.
+    #MC The unstable packages can be accessed like `pkgs.pkgsu.hello`.
     pkgsu = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
   };
 }
