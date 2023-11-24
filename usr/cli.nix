9786b81d3009df7fcbdd74d3a33b34b51d574b47
xieby1 <xieby1@outlook.com>
2023-11-24 12:55:07 +0000

    usr: cli: replace ps with pidof for sshd-start and tailscale

--- usr/cli.nix
+++ usr/cli.nix
@@ -120,7 +120,7 @@ in
         "";
     programs.bash.bashrcExtra = lib.optionalString config.isNixOnDroid ''
       # start sshd
-      if [[ -z "$(ps|grep sshd-start|grep -v grep)" ]]; then
+      if [[ -z "$(pidof sshd-start)" ]]; then
           tmux new -d -s sshd-start sshd-start
       fi
     '';
--- usr/cli/tailscale.nix
+++ usr/cli/tailscale.nix
@@ -67,7 +67,7 @@ let
     };
     programs.bash.bashrcExtra = lib.optionalString config.isNixOnDroid ''
       # start tailscale-${suffix}
-      if [[ -z "$(ps -e -o cmd|grep tailscaled-${suffix}|grep -v grep)" ]]; then
+      if [[ -z "$(pidof tailscaled-${suffix})" ]]; then
           tmux new -d -s tailscaled-${suffix} tailscaled-${suffix}
       fi
     '';
