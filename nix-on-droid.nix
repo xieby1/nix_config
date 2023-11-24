d2a577a54f6065dc44d1dea3d35f126c5c83896f
xieby1 <xieby1@outlook.com>
2023-11-24 12:57:47 +0000

    nix-on-droid: restruct

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -1,25 +1,65 @@
 { pkgs, config, ... }:
-let
-  sshdTmpDirectory = "${config.user.home}/sshd-tmp";
-  sshdDirectory = "${config.user.home}/sshd";
-  pathToPubKey = "${config.user.home}/.ssh/id_rsa.pub";
-  port = 8022;
-in
-{
-  # Simply install just the packages
-  environment.packages = with pkgs; [
-    # User-facing stuff that you really really want to have
-    vim  # or some other editor, e.g. nano or neovim
 
+{
+  imports = [(let
+    sshdTmpDirectory = "${config.user.home}/sshd-tmp";
+    sshdDirectory = "${config.user.home}/sshd";
+    pathToPubKey = "${config.user.home}/.ssh/id_rsa.pub";
+    port = 8022;
     # sshd-start script
     # refers to: https://github.com/t184256/nix-on-droid/wiki/SSH-access
-    (pkgs.writeScriptBin "sshd-start" ''
+    sshd-start = pkgs.writeScriptBin "sshd-start" ''
       #!${pkgs.runtimeShell}
 
       echo "Starting sshd in non-daemonized way on port ${toString port}"
       ${pkgs.openssh}/bin/sshd -f "${sshdDirectory}/sshd_config" -D
-    '')
+    '';
+  in {
+    environment.packages = with pkgs; [
+      sshd-start
+    ];
+    build.activation.sshd = ''
+      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
+      $DRY_RUN_CMD cat ${pathToPubKey} > "${config.user.home}/.ssh/authorized_keys"
 
+      if [[ ! -d "${sshdDirectory}" ]]; then
+        $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
+        $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
+
+        $VERBOSE_ECHO "Generating host keys..."
+        $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
+
+        $VERBOSE_ECHO "Writing sshd_config..."
+        $DRY_RUN_CMD echo -e "HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n" > "${sshdTmpDirectory}/sshd_config"
+
+        $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
+      fi
+    '';
+  }) ({
+    build.activation.termux = ''
+      DIR=${config.user.home}/.termux
+      mkdir -p ''$DIR
+      symlink()
+      {
+        if [[ -e ''$1 && ! -e ''$2 ]]
+        then
+          #echo "ln -s ''$1 ''$2"
+          ln -s ''$1 ''$2
+        fi
+      }
+      SRC=${config.user.home}/Gist/Config/termux.properties
+      DST=''${DIR}/termux.properties
+      symlink ''$SRC ''$DST
+      SRC=${config.user.home}/Gist/Config/colors.properties
+      DST=''${DIR}/colors.properties
+      symlink ''$SRC ''$DST
+    '';
+  })];
+
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    vim  # or some other editor, e.g. nano or neovim
     # Some common stuff that people expect to have
     diffutils
     findutils
@@ -51,59 +91,6 @@ in
   # Read the changelog before changing this value
   system.stateVersion = "21.11";
 
-  # After installing home-manager channel like
-  #   nix-channel --add https://github.com/rycee/home-manager/archive/release-21.11.tar.gz home-manager
-  #   nix-channel --update
   # you can configure home-manager in here like
   home-manager.config = import ./home.nix;
-  #home-manager.config =
-  #  { pkgs, lib, ... }:
-  #  {
-  #    # Read the changelog before changing this value
-  #    home.stateVersion = "21.11";
-  #
-  #    # Use the same overlays as the system packages
-  #    nixpkgs = { inherit (config.nixpkgs) overlays; };
-  #
-  #    # insert home-manager config
-  #  };
-  # If you want the same pkgs instance to be used for nix-on-droid and home-manager
-  #home-manager.useGlobalPkgs = true;
-  build.activation.sshd = ''
-    $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${config.user.home}/.ssh"
-    $DRY_RUN_CMD cat ${pathToPubKey} > "${config.user.home}/.ssh/authorized_keys"
-
-    if [[ ! -d "${sshdDirectory}" ]]; then
-      $DRY_RUN_CMD rm $VERBOSE_ARG --recursive --force "${sshdTmpDirectory}"
-      $DRY_RUN_CMD mkdir $VERBOSE_ARG --parents "${sshdTmpDirectory}"
-
-      $VERBOSE_ECHO "Generating host keys..."
-      $DRY_RUN_CMD ${pkgs.openssh}/bin/ssh-keygen -t rsa -b 4096 -f "${sshdTmpDirectory}/ssh_host_rsa_key" -N ""
-
-      $VERBOSE_ECHO "Writing sshd_config..."
-      $DRY_RUN_CMD echo -e "HostKey ${sshdDirectory}/ssh_host_rsa_key\nPort ${toString port}\n" > "${sshdTmpDirectory}/sshd_config"
-
-      $DRY_RUN_CMD mv $VERBOSE_ARG "${sshdTmpDirectory}" "${sshdDirectory}"
-    fi
-  '';
-  build.activation.termux = ''
-    DIR=${config.user.home}/.termux
-    mkdir -p ''$DIR
-    symlink()
-    {
-      if [[ -e ''$1 && ! -e ''$2 ]]
-      then
-        #echo "ln -s ''$1 ''$2"
-        ln -s ''$1 ''$2
-      fi
-    }
-    SRC=${config.user.home}/Gist/Config/termux.properties
-    DST=''${DIR}/termux.properties
-    symlink ''$SRC ''$DST
-    SRC=${config.user.home}/Gist/Config/colors.properties
-    DST=''${DIR}/colors.properties
-    symlink ''$SRC ''$DST
-  '';
 }
-
-# vim: ft=nix
