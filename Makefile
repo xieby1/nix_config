1e4c5d95ed4404893678028816fbb9281269f7a3
xieby1 <xieby1@outlook.com>
2023-11-24 12:46:36 +0800

    use markcode to doc for all .nix file

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 all: SUMMARY.md
 	mdbook build
 
+NIXs = $(shell find . -name "*.nix" -not -path "./book*")
+NIX_MDs = $(addsuffix .md,${NIXs})
+%.md: %
+	markcode $< > $@
+
 .PHONY: SUMMARY.md
-SUMMARY.md: .genSUMMARY.sh
+SUMMARY.md: .genSUMMARY.sh ${NIX_MDs}
 	./$< > $@
--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
 let
   name = "nix_config";
   pkgs = import <nixpkgs> {};
+  markcode = pkgs.callPackage (
+    pkgs.fetchFromGitHub {
+      owner = "xieby1";
+      repo = "markcode";
+      rev = "aab7cdedae2a90fd1dea56a81fdf85689ef9b537";
+      hash = "sha256-HCmtCnk3Q/Bng7QZzMm5C7nrKKNyz43yWLdv/CSuFyM=";
+    }
+  ) {};
 in pkgs.mkShell {
   inherit name;
   buildInputs = with pkgs; [
     mdbook
+    markcode
   ];
 }
