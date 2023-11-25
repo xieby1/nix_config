71ec77f1aaf1dccacda55a78a307981d45b2b288
xieby1 <xieby1@outlook.com>
2023-11-25 15:39:34 +0800

    markcode: add file link to home.nix

--- home.nix
+++ home.nix
@@ -2,9 +2,9 @@
 #MC
 #MC `home.nix`是home-manager的入口配置文件，它导入了3个nix文件：
 #MC
-#MC * `./opt.nix`：我定义的全局变量
-#MC * `./usr/cli.nix`：我的所有CLI程序的配置
-#MC * `./usr/gui.nix`：我的所有GUI程序的配置
+#MC * [`./opt.nix`](./opt.nix.md)：我定义的全局变量
+#MC * [`./usr/cli.nix`](./usr/cli.nix.md)：我的所有CLI程序的配置
+#MC * [`./usr/gui.nix`](./usr/gui.nix.md)：我的所有GUI程序的配置
 #MC
 #MC 因为部分系统不需要GUI程序，比如安卓手机nix-on-droid或是树莓派等。
 #MC 我利用环境变量`DISPLAY`用于判断是否导入GUI程序的配置。
