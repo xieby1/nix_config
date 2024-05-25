ca537807f8ef14f63b348aa0133d9158e75f6267
xieby1 <xieby1@outlook.com>
2024-05-25 11:27:11 +0800

    usr: cli: clashctl: fix name (original name is tooooo looooong)

--- usr/cli/clashctl.nix
+++ usr/cli/clashctl.nix
@@ -2,19 +2,18 @@
 , rustPlatform
 , fetchFromGitHub
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "clashctl";
-  version = "b09e1faf80f1a25fa855499d8b34d36491e5a081";
+rustPlatform.buildRustPackage {
+  name = "clashctl";
 
   src = fetchFromGitHub {
     # owner = "https://github.com/George-Miao/clashctl";
     owner = "George-Miao";
-    repo = pname;
-    rev = version;
+    repo = "clashctl";
+    rev = "b09e1faf80f1a25fa855499d8b34d36491e5a081";
     hash = "sha256-c7y64SsZEKdC8+umCY8+XBwxAHxn4YpqR48ASbHpkdM=";
   };
 
-  cargoHash = "sha256-VEQ1amZxCTV4jefN20d8VV85/IaJivl0s8RVclV+jqg=";
+  cargoHash = "sha256-2d+phmMJeHZsJy300GwUAnh8KTGzpHCCIg8x4Wr8ytE=";
 
   doCheck = false;
 
