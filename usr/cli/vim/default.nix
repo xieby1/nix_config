bcd968d978b136cc1656ce1fc1b5784b7b0817c9
xieby1 <xieby1@outlook.com>
2024-06-02 14:27:11 +0800

    usr: cli: split vim: nvim-treesitter

--- usr/cli/vim/default.nix
+++ usr/cli/vim/default.nix
@@ -1,36 +1,6 @@
 { config, pkgs, stdenv, lib, ... }:
 
 let
-  # pluginWithConfigType is not displayed in `man home-configuration.nix`
-  # see its avaiable config in `home-manager/modules/programs/neovim.nix`
-  my-nvim-treesitter = {
-    # Available languages see:
-    #   https://github.com/nvim-treesitter/nvim-treesitter
-    # see `pkgs.tree-sitter.builtGrammars.`
-    # with `tree-sitter-` prefix and `-grammar` suffix removed
-    plugin = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: with p; [
-      c
-      cpp
-      python
-      markdown
-    ]);
-    type = "lua";
-    config = ''
-      require 'nvim-treesitter.configs'.setup {
-        -- TODO: https://github.com/NixOS/nixpkgs/issues/189838
-        -- ensure_installed = {"c", "cpp", "python", "markdown"},
-        ensure_installed = {},
-        sync_install = false,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-          disable = {
-            "nix",
-          },
-        },
-      }
-    '';
-  };
   my-nvim-config-local = {
     plugin = pkgs.vimUtils.buildVimPlugin {
       name = "nvim-config-local";
@@ -359,6 +329,7 @@ in
     ./nvim-cmp.nix
     ./vim-mark.nix
     ./DrawIt.nix
+    ./nvim-treesitter.nix
   ];
 
   # neovim
@@ -506,7 +477,6 @@ in
       vim-markdown-toc
       my-vista-vim
       vim-commentary
-      my-nvim-treesitter
       # javascript lsp support
       # coc-tsserver
       my-vim-markdown # format table
