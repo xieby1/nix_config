bb219a948642caf24a62cc803d52711eb1d76108
xieby1 <xieby1@outlook.com>
2024-06-03 11:32:14 +0800

    usr: cli: split vim: color-scheme

--- usr/cli/vim/default.nix
+++ usr/cli/vim/default.nix
@@ -1,40 +1,6 @@
 { config, pkgs, stdenv, lib, ... }:
 
 let
-  my-color-scheme = {
-    plugin = pkgs.vimPlugins.sonokai;
-    config = ''
-      nnoremap <leader>c :set termguicolors!<CR>
-      set termguicolors
-
-      let g:sonokai_transparent_background = 1
-
-      let g:sonokai_colors_override = {
-      \ 'black':    ['#111215', '237'],
-      \ 'bg0':      ['#22232a', '235'],
-      \ 'bg1':      ['#33353f', '236'],
-      \ 'bg2':      ['#444754', '236'],
-      \ 'bg3':      ['#555869', '237'],
-      \ 'bg4':      ['#666a7e', '237'],
-      \ 'grey':     ['#a5a5a6', '246'],
-      \ 'grey_dim': ['#787879', '240'],
-      \}
-
-      " custom sonokai,
-      " see section "How to use custom colors" of `:h sonokai.vim`
-      function! s:sonokai_custom() abort
-        let l:palette = sonokai#get_palette('default', {})
-        call sonokai#highlight('StatusLine', l:palette.black, l:palette.fg, 'bold')
-        call sonokai#highlight('StatusLineNC', l:palette.black, l:palette.grey, 'bold')
-      endfunction
-      augroup SonokaiCustom
-        autocmd!
-        autocmd ColorScheme sonokai call s:sonokai_custom()
-      augroup END
-
-      colorscheme sonokai
-    '';
-  };
   my-hbac = {
     plugin = pkgs.vimUtils.buildVimPlugin {
       name = "hbac.nvim";
@@ -181,6 +147,7 @@ in
     ./vim-markdown.nix
     ./vim-hexokinase.nix
     ./gitsigns-nvim.nix
+    ./color-scheme.nix
   ];
 
   # neovim
@@ -329,7 +296,6 @@ in
       # coc-tsserver
       tabular
       vim-plugin-AnsiEsc
-      my-color-scheme
       my-hbac
       my-winshift-nvim
       my-smartyank-nvim
