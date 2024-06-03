661df760aba8c25b8137eba325e70acbd0b394c5
xieby1 <xieby1@outlook.com>
2024-06-03 15:58:54 +0800

    update 24.05: usr: cli: disable searxng

--- usr/cli.nix
+++ usr/cli.nix
@@ -89,7 +89,9 @@ in
     ./cli/vim
     ./cli/clash.nix
     ./cli/tailscale.nix
-    ./cli/searxng.nix
+    # When init searxng, it throws `ERROR:searx.engines.wikidata: Fail to initialize`
+    # I have no idea, so disable it.
+    # ./cli/searxng.nix
   ] ++ [{ # functions & attrs
     home.packages = [
       pkgs.fzf
