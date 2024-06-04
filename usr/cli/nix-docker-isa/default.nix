688f8f962a8b6cc3ef1be625395b3c310ebd8631
xieby1 <xieby1@outlook.com>
2024-06-04 18:51:41 +0800

    usr/cli/nix-docker-isa: not dependent on docker-nixpkgs

--- usr/cli/nix-docker-isa/default.nix
+++ usr/cli/nix-docker-isa/default.nix
@@ -1,56 +1,65 @@
-#MC # Nix Docker for Multiple ISAs
-# TODO: support multiple ISAs (currently only riscv64)
-# currently: this riscv64 nix docker can `nix-env -iA nixpkgs.hello`,
-# which is completely built from source including toolchains (stdenv) in riscv64.
+#MC # 🐳Nix Docker🐋 for Multiple ISAs
+#MC
+#MC This script is inspired by https://github.com/nix-community/docker-nixpkgs/images/nix
+#MC
+#MC TODO: support multiple ISAs (currently only riscv64)
+#MC currently: this riscv64 nix docker can `nix-env -iA nixpkgs.hello`,
+#MC which is completely built from source including toolchains (stdenv) in riscv64.
 let
   pkgs = import <nixpkgs> {};
   # pkgsCross = import <nixpkgs> {};
   pkgsCross = pkgs.pkgsCross.riscv64;
-  docker-nixpkgs-src = pkgs.fetchFromGitHub {
-    owner = "nix-community";
-    repo = "docker-nixpkgs";
-    rev = "bfac57f18680c9b2927b9c85a17e5b4cd89c27f2";
-    hash = "sha256-fn9wmhwIFCuTziPZZ0HlJxqNEnOyjdaiHGrvD2niXOU=";
-  };
-  docker-image = import "${docker-nixpkgs-src}/images/nix" {
-    inherit (pkgs) dockerTools cacert;
-    inherit (pkgsCross)
-      bashInteractive
-      coreutils
-      curl
-      gnutar
-      gzip
-      iana-etc
-      nix
-      openssh
-      xz
-    ;
-    gitReallyMinimal = pkgsCross.gitMinimal;
-    extraContents = [
-      # (pkgs.writeTextFile {
-      #   name = "nix.conf";
-      #   text = ''
-      #     filter-syscalls = false
-      #   '';
-      #   destination = "/etc/nix/nix.conf";
-      # })
-      ./imageFiles
-    ];
+  name = "nix-${pkgsCross.stdenv.system}";
+  image = pkgs.dockerTools.buildImageWithNixDb {
+    inherit name;
+    copyToRoot = pkgs.buildEnv {
+      name = "image-root";
+      paths = (with pkgsCross; [
+        bashInteractive
+        cacert
+        coreutils
+        file
+        gitMinimal
+        gnutar
+        nix
+        openssh
+        vim
+        wget
+      ]) ++ [
+        ./imageFiles
+      ];
+    };
+    extraCommands = ''
+      # for /usr/bin/env
+      mkdir usr
+      ln -s bin usr/bin
+
+      # make sure /tmp exists
+      mkdir -m 1777 tmp
+    '';
+    config = {
+      Cmd = [ "/bin/bash" ];
+      Env = [
+        "NIX_BUILD_SHELL=/bin/bash"
+        "PAGER=cat"
+        "PATH=/usr/bin:/bin"
+        "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+        "USER=root"
+      ];
+    };
   };
 in pkgs.writeShellScriptBin "nix-docker-riscv64" ''
   command -v podman &> /dev/null || echo "podman not found TODO: install" || exit 1
 
-  # TODO: add tag support to docker-nixpkgs
-  outName="$(basename ${docker-image})"
+  outName="$(basename ${image})"
   outHash=$(echo "$outName" | cut -d - -f 1)
-  nixName=${pkgsCross.nix.name}
-  imageName=localhost/$nixName:$outHash
+  imageName=localhost/${name}:$outHash
 
   # check whether image has been loaded
-  podman images $imageName | grep $nixName | grep $outHash &> /dev/null
+  podman images $imageName | grep ${name} | grep $outHash &> /dev/null
   # image has not been loaded, then load it
   if [[ $? != 0 ]]; then
-    podman load -i ${docker-image}
+    podman load -i ${image}
   fi
 
 
@@ -60,7 +69,7 @@ in pkgs.writeShellScriptBin "nix-docker-riscv64" ''
       BINFMTS+=" -v $(realpath $binfmt):$binfmt"
   done
 
-  containerName=$nixName-$outHash
+  containerName=${name}-$outHash
   # run container
   podman run -it \
     --name=$containerName \
