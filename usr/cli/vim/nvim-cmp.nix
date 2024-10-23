d3b6d2d75514bc7e62b3238b8ea69eadb6fa0b1c
xieby1 <xieby1@outlook.com>
2024-10-23 11:14:50 +0800

    usr: cli: vim: nvim-cmp: disable preselect

--- usr/cli/vim/nvim-cmp.nix
+++ usr/cli/vim/nvim-cmp.nix
@@ -8,6 +8,7 @@ let
       local cmp = require'cmp'
 
       cmp.setup({
+        preselect = cmp.PreselectMode.None,
         snippet = {
           -- REQUIRED - you must specify a snippet engine
           expand = function(args)
