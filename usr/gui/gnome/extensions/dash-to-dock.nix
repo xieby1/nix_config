94916dff2a459242a67a36c2253cd99eb2ed9408
xieby1 <xieby1@users.noreply.github.com>
2025-03-08 02:06:55 +0000

    deploy: 871c627149cf8d88460660fa2bbcd64d26f388c7

---
+++ usr/gui/gnome/extensions/dash-to-dock.nix
@@ -0,0 +1,18 @@
+#MC # dash-to-dock
+{ pkgs, ... }: {
+  home.packages = [
+    pkgs.gnomeExtensions.dash-to-dock
+  ];
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+    };
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      click-action="focus-or-appspread";
+      transparency-mode = "FIXED";
+      background-opacity = 0.4;
+    };
+  };
+}
