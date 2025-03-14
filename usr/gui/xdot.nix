86e7d3ec12e3378a22ae6e4b155c56c8e48e84de
xieby1 <xieby1@outlook.com>
2025-03-15 00:03:26 +0800

    usr: gui: xdot: add icon

--- usr/gui/xdot.nix
+++ usr/gui/xdot.nix
@@ -10,6 +10,14 @@ let
         name = "xdot";
         desktopName = "xdot";
         exec = "xdot %U";
+        icon = builtins.toFile "cheatsheet.svg" ''
+          <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100%" height="100%" rx="20%" ry="20%" fill="#F5F5F5"/>
+            <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="18" font-weight="bold" fill="#333333">
+              xdot
+            </text>
+          </svg>
+        '';
   })];};
 in {
   home.packages = [
