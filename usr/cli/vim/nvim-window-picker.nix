05d1d83be9e2f36bd8462d48346ea7f066c0b198
xieby1 <xieby1@outlook.com>
2025-04-11 13:16:43 +0800

    usr: cli: vim: nvim-window-picker: customize appearance

--- usr/cli/vim/nvim-window-picker.nix
+++ usr/cli/vim/nvim-window-picker.nix
@@ -7,6 +7,24 @@
       config = ''
         require 'window-picker'.setup({
           selection_chars = 'abcdefghijklmnopqrstuvwxyz',
+          picker_config = {
+            statusline_winbar_picker = {
+              selection_display = function(char, windowid)
+                -- https://unix.stackexchange.com/questions/224771/what-is-the-format-of-the-default-statusline
+                return '[' .. char .. '] %<%f %h%w%m%r%=%-14.(%l,%c%V%) %P'
+              end,
+            },
+          },
+          show_prompt = false,
+          highlights = {
+            statusline = {
+              unfocused = {
+                fg = '#2c2e34',
+                bg = '#7f8490',
+                bold = true,
+              },
+            },
+          },
         })
         vim.keymap.set('n', '<C-j>', function()
           vim.api.nvim_set_current_win( require('window-picker').pick_window() )
