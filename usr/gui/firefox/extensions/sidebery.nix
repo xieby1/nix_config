560fa835584746e4cb1174896a62274e97c51df1
xieby1 <xieby1@outlook.com>
2025-05-27 14:09:48 +0800

    usr: gui: firefox: extensions: sidebery: transparent floating failed

--- usr/gui/firefox/extensions/sidebery.nix
+++ usr/gui/firefox/extensions/sidebery.nix
@@ -4,6 +4,45 @@
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         sidebery
       ];
+      # transparent floating sidebar
+      # Why not use it?
+      # Because: I tried two ways to adjust the height of sidebar dynamically, but all failed:
+      # * user.js cannot inject js code
+      # * css counter cannot used in height
+      #
+      #settings = {
+      #  "browser.tabs.allow_transparent_browser" = true;
+      #};
+      #userChrome = ''
+      #  #sidebar-box {
+      #    height: 90%;
+      #    top: 5%;
+      #    right: 0;
+      #    position: absolute;
+      #    z-index: 99;
+      #  }
+      #  #browser,
+      #  tabbox#tabbrowser-tabbox,
+      #  #tabbrowser-tabpanels,
+      #  #sidebar-box,
+      #  #sidebar,
+      #  window#webextpanels-window {
+      #    background-color: transparent !important;
+      #    background-image: none !important;
+      #  }
+      #'';
+      #userContent = ''
+      #  @-moz-document regexp("^moz-extension://.*/sidebar/sidebar.html") {
+      #    :root {
+      #      background-color: transparent !important;
+      #    }
+      #    #root.root {
+      #      --frame-bg: transparent !important;
+      #      --toolbar-bg: black !important;
+      #      --frame-el-bg: black !important;
+      #    }
+      #  }
+      #'';
       settings = {
         "sidebar.position_start" = false; # sidebar on the right
       };
