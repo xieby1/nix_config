6eb8cb3a42d8827c3add5276bcf2bd5f97286980
xieby1 <xieby1@outlook.com>
2025-06-04 12:05:15 +0800

    usr: gui: firefox: extensions: darkreader: add disables

--- usr/gui/firefox/extensions/darkreader.nix
+++ usr/gui/firefox/extensions/darkreader.nix
@@ -1,16 +1,21 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+  darkreader = pkgs.nur.repos.rycee.firefox-addons.darkreader;
+in {
   programs.firefox = {
     profiles.xieby1 = {
-      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-        darkreader
-      ];
+      extensions.packages = [ darkreader ];
     };
   };
-  firefox-extensions.xieby1.browser-extension-data."${pkgs.nur.repos.rycee.firefox-addons.darkreader.addonId}" = {
+  firefox-extensions.xieby1.browser-extension-data."${darkreader.addonId}" = {
     storage = {
       schemeVersion = 2;
       syncSettings = false;
       disabledFor = [
+        # markdown-preview-nvim: x.x.x.x:7768
+        # markdown_revealjs daemon: x.x.x.x:7782
+        "/.*:77[0-9][0-9]/"
+        # markdown_revealjs
+        "//home/.*/slides/index.html/"
       ];
     };
   };
