9c4197beb7e8b462aa7e0cc52e24c180452d1b67
xieby1 <xieby1@outlook.com>
2025-06-04 16:28:38 +0800

    usr: cli: vim: markdown-preview-nvim: use 77-prefixed port

--- usr/cli/vim/markdown-preview-nvim.nix
+++ usr/cli/vim/markdown-preview-nvim.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }: {
   programs.neovim = {
     plugins = [{
-      plugin = pkgs.vimPlugins.markdown-preview-nvim;
+      plugin = pkgs.vimPlugins.markdown-preview-nvim.overrideAttrs (old: {
+        # set port to 7700~7799
+        postPatch = ''
+          sed -i 's/port = port ||.*/port = port || (7700 + Number(`''${Date.now()}`.slice(-2)))/' app/server.js
+        '';
+      });
       config = ''
-        " ascii code for MD
-        let g:mkdp_port = '7768'
         " allow LAN
         let g:mkdp_open_to_the_world = 1
         let g:mkdp_theme = 'light'
