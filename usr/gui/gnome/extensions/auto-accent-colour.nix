e60868bff512e774918a731d3d88b5709cf17557
xieby1 <xieby1@outlook.com>
2025-06-05 11:56:24 +0800

    usr: gui: gnome: extensions: auto-accent-colour: fix gjs

--- usr/gui/gnome/extensions/auto-accent-colour.nix
+++ usr/gui/gnome/extensions/auto-accent-colour.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }: {
-  home.packages = [
-    pkgs.gnomeExtensions.auto-accent-colour
-  ];
+  home.packages = [(
+    pkgs.gnomeExtensions.auto-accent-colour.overrideAttrs (old: {
+      postPatch = ''
+        substituteInPlace extension.js --replace "gjs" "${pkgs.gjs}/bin/gjs"
+      '';
+    })
+  )];
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
