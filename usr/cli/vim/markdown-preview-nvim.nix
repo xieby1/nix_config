a4729f107c0258c269e437ede662784c059501c7
xieby1 <xieby1@outlook.com>
2025-06-10 10:35:10 +0800

    usr: cli: vim: markdown-preview-nvim: patch line-height of <pre>

--- usr/cli/vim/markdown-preview-nvim.nix
+++ usr/cli/vim/markdown-preview-nvim.nix
@@ -2,10 +2,15 @@
   programs.neovim = {
     plugins = [{
       plugin = pkgs.vimPlugins.markdown-preview-nvim.overrideAttrs (old: {
+        postPatch = toString [
         # set port to 7700~7799
-        postPatch = ''
+        ''
           sed -i 's/port = port ||.*/port = port || (7700 + Number(`''${Date.now()}`.slice(-2)))/' app/server.js
-        '';
+        ''
+        # set line-height of <pre></pre>
+        ''
+          sed -i 's/line-height: 1.45;/line-height: 1em;/' app/_static/markdown.css
+        ''];
       });
       config = ''
         " allow LAN
