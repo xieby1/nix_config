10441ceb23cea95533458671449aa1686db05a08
xieby1 <xieby1@outlook.com>
2025-06-10 14:29:33 +0800

    usr: cli: vim: markdown-preview-nvim: open by using chromium

--- usr/cli/vim/markdown-preview-nvim.nix
+++ usr/cli/vim/markdown-preview-nvim.nix
@@ -16,6 +16,16 @@
         " allow LAN
         let g:mkdp_open_to_the_world = 1
         let g:mkdp_theme = 'light'
+
+        " Use chromium-like browser to open rendered markdown instead of firefox,
+        " because firefox only copy css in selected region, causing lost of styles!
+        " See same bug report here:
+        " https://discourse.mozilla.org/t/copy-rich-text-from-firefox-does-not-capture-style/97862/3
+        function OpenMarkdownPreview (url)
+          " run chromium in app mode in background
+          execute "silent ! chromium --app='" . a:url . "' &"
+        endfunction
+        let g:mkdp_browserfunc = 'OpenMarkdownPreview'
       '';
     }];
   };
