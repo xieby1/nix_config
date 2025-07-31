5dd748d5e9e6fd848508217b8f9151dd95f62216
xieby1 <xieby1@outlook.com>
2025-07-31 17:32:51 +0800

    usr: cli: vim: nvim-nav: replace space with separator

--- usr/cli/vim/nvim-nav.nix
+++ usr/cli/vim/nvim-nav.nix
@@ -11,11 +11,11 @@
         }
         -- https://unix.stackexchange.com/questions/224771/what-is-the-format-of-the-default-statusline
         -- %<%f %h%w%m%r%=%-14.(%l,%c%V%) %P
-        vim.o.statusline = "%<%f %h%w%m%r " ..
+        vim.o.statusline = "%<%f %h%w%m%r┇" ..
           -- set maximum width of nvim-navic str: winwidth-40
           -- exec %{%...%} results in '%.xx(yyyy%)', where xx is the max width, yyyy is the navic str
           "%{%'%.' .. (winwidth(0)-40) .. '(' .. v:lua.require'nvim-navic'.get_location() .. '%)'%}" ..
-          " %=%-8.(%l,%c%) %P"
+          "%=┇%-8.(%l,%c%) %P"
       '';
     }{
       plugin = pkgs.vimPlugins.nvim-navbuddy;
