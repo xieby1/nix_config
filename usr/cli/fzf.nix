1c8f436ed982ccc79ce1169e3c49ef6ce53ef7e3
xieby1 <xieby1@outlook.com>
2025-08-05 09:49:45 +0800

    usr: cli: fzf: fix fzf-doc background

--- usr/cli/fzf.nix
+++ usr/cli/fzf.nix
@@ -53,7 +53,7 @@
       # use nohup to run bash command in background and exit
       ## https://superuser.com/questions/448445/run-bash-script-in-background-and-exit-terminal
       # nohup not recognize bash alias like `o`, it's necessary to call bash
-      nohup bash -i "$CMD" "$FILE" &
+      nohup bash -ic "$CMD \"$FILE\"" &
     else
       # FILE name may contain space, quote FILE name
       eval "$CMD" \"$FILE\"
