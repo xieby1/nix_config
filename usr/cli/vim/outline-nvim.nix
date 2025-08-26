99dd1e3fbaff475625a093f02c5fb2868696317d
xieby1 <xieby1@outlook.com>
2025-08-26 14:18:24 +0800

    usr: cli: vim: outline-nvim: add outline-treesitter-provider.nvim

--- usr/cli/vim/outline-nvim.nix
+++ usr/cli/vim/outline-nvim.nix
@@ -3,8 +3,18 @@
     plugins = [{
       plugin = pkgs.vimPlugins.outline-nvim;
       type = "lua";
-      config = ''
+      config = /*lua*/ ''
         require("outline").setup({
+          -- Outline.nvim seems not support setup multiple times
+          -- So I merge outline-treesitter-provider's config here
+          providers = {
+            -- append "treesitter" to original priority
+            priority = (function() -- start of a local scope
+              local original = require("outline.config").defaults.providers.priority
+              table.insert(original, "treesitter")
+              return original
+            end)(), -- end of a local scope
+          },
           outline_window = {
             relative_width = false,
           },
@@ -19,6 +29,17 @@
           },
         })
       '';
-    }];
+    }(
+      pkgs.vimUtils.buildVimPlugin {
+        name = "outline-treesitter-provider.nvim";
+        src = pkgs.fetchFromGitHub {
+          owner = "epheien";
+          repo = "outline-treesitter-provider.nvim";
+          rev = "22dda7329cf608368cbf725effd43daf98c27f32";
+          hash = "sha256-PFRiaQPxh+PPFGF1+yMIJIM/tGDOH7CO0xyoayDn78I=";
+        };
+        doCheck = false;
+      }
+    )];
   };
 }
