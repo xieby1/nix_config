a331aa3e44485669b2e5dd9abc278467eed55411
xieby1 <xieby1@outlook.com>
2025-09-16 16:54:19 +0800

    usr: cli: vim: blink-cmp: alt+<num> accept

--- usr/cli/vim/blink-cmp.nix
+++ usr/cli/vim/blink-cmp.nix
@@ -42,12 +42,32 @@
           ['<Up>']   = { function(cmp) return cmp.select_prev({ auto_insert = false }) end, 'fallback' },
           ['<Down>'] = { function(cmp) return cmp.select_next({ auto_insert = false }) end, 'fallback' },
           ['<CR>'] = { 'accept', 'fallback' },
+          ['<A-1>'] = { function(cmp) cmp.accept({ index = 1 }) end },
+          ['<A-2>'] = { function(cmp) cmp.accept({ index = 2 }) end },
+          ['<A-3>'] = { function(cmp) cmp.accept({ index = 3 }) end },
+          ['<A-4>'] = { function(cmp) cmp.accept({ index = 4 }) end },
+          ['<A-5>'] = { function(cmp) cmp.accept({ index = 5 }) end },
+          ['<A-6>'] = { function(cmp) cmp.accept({ index = 6 }) end },
+          ['<A-7>'] = { function(cmp) cmp.accept({ index = 7 }) end },
+          ['<A-8>'] = { function(cmp) cmp.accept({ index = 8 }) end },
+          ['<A-9>'] = { function(cmp) cmp.accept({ index = 9 }) end },
+          ['<A-0>'] = { function(cmp) cmp.accept({ index = 10 }) end },
         },
         completion = {
           documentation = { auto_show = true },
           menu = {
             draw = {
-              columns = { {'kind_icon'}, {'label', 'label_description', gap = 1}, {'source_name'}, },
+              columns = {
+                {'item_idx'},
+                {'kind_icon'}, {'label', 'label_description', gap = 1},
+                {'source_name'},
+              },
+              components = {
+                item_idx = {
+                  text = function(ctx) return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or tostring(ctx.idx) end,
+                  highlight = 'BlinkCmpSource' -- optional, only if you want to change its color
+                },
+              },
             },
           },
           list = { selection = { preselect = false }, },
--- usr/gui/kitty/default.nix
+++ usr/gui/kitty/default.nix
@@ -118,17 +118,6 @@
       map ctrl+kp_subtract change_font_size all -2.0
       map ctrl+0 change_font_size all 0
 
-      map alt+1 goto_tab 1
-      map alt+2 goto_tab 2
-      map alt+3 goto_tab 3
-      map alt+4 goto_tab 4
-      map alt+5 goto_tab 5
-      map alt+6 goto_tab 6
-      map alt+7 goto_tab 7
-      map alt+8 goto_tab 8
-      map alt+9 goto_tab 9
-      map alt+0 goto_tab 99
-
       map ctrl+shift+t new_tab_with_cwd
       map ctrl+shift+n new_os_window_with_cwd
 
