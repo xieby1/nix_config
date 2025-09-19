a313469c593e0927c0506cdfecb15084319978bd
xieby1 <xieby1@outlook.com>
2025-09-19 10:55:46 +0800

    usr: cli: vim: nvim-lspconfig: scala: update settings

--- usr/cli/vim/nvim-lspconfig/scala.nix
+++ usr/cli/vim/nvim-lspconfig/scala.nix
@@ -1,3 +1,7 @@
+# If metals throw errors, check <workspace>/.metals/metals.log
+# Try following steps:
+# * clean .metals/, ~/.bloop/, ~/.cache/mill
+# * check all dependent libs have been downloaded to workspace, e.g. update git submodule
 { config, pkgs, ... }: let
   jre-with-proxy = let
     java-with-proxy = pkgs.writeShellScript "java" ''
@@ -19,6 +23,25 @@ in {
     extraLuaConfig = /*lua*/ ''
       vim.lsp.enable("metals")
     '';
-    extraPackages = [ (pkgs.metals.override {jre = jre-with-proxy;}) ];
+    extraPackages = [
+      ((pkgs.metals.override {
+        jre=jre-with-proxy;
+      }).overrideAttrs (old: {
+        extraJavaOpts = toString [
+          old.extraJavaOpts
+          /*
+            User configuration options can optionally be provided via server properties using the -Dmetals. prefix.
+            For more user configuration, see: https://scalameta.org/metals/docs/integrations/new-editor/#metals-user-configuration
+          */
+          # use mill in system, preventing the situation where I can compile but I cannot use metals
+          # If mill is provided:
+          "-Dmetals.millScript=mill"
+
+          # If mill command (specified by millScript) is not found,
+          # then metals will use java to download mill
+          "-Dmetals.javaHome=${jre-with-proxy}"
+        ];
+      }))
+    ];
   };
 }
