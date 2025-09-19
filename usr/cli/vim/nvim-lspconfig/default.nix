307f1e7115de168e7ce5a9d11c4a48b15b8dc0b9
xieby1 <xieby1@outlook.com>
2025-09-19 12:50:01 +0800

    usr: cli: vim: nvim-lspconfig: use telescope lsp picker

--- usr/cli/vim/nvim-lspconfig/default.nix
+++ usr/cli/vim/nvim-lspconfig/default.nix
@@ -40,18 +40,14 @@
         vim.api.nvim_create_autocmd('LspAttach', {
           group = vim.api.nvim_create_augroup('UserLspConfig', {}),
           callback = function(ev)
-            -- Enable completion triggered by <c-x><c-o>
-            vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-            -- Buffer local mappings.
-            -- See `:help vim.lsp.*` for documentation on any of the below functions
             local opts = { buffer = ev.buf }
-            -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', 'gd', builtin.lsp_definitions, opts)
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, opts)
+            vim.keymap.set('n', 'gr', builtin.lsp_references, opts)
+            vim.keymap.set('n', 'gi', builtin.lsp_implementations, opts)
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, opts)
             vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
           end,
         })
