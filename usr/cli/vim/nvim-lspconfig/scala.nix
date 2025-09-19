b83af4abf947363017b5dd2ada9075250fac0eeb
xieby1 <xieby1@outlook.com>
2025-09-19 09:41:48 +0800

    usr: cli: vim: nvim-lspconfig: combine jre_with_proxy.nix and scala.nix

--- usr/cli/vim/nvim-lspconfig/scala.nix
+++ usr/cli/vim/nvim-lspconfig/scala.nix
@@ -1,14 +1,24 @@
 { config, pkgs, ... }: let
-  jre_with_proxy = pkgs.callPackage ./jre_with_proxy.nix {
-    jre = pkgs.openjdk_headless;
-    proxyHost = "127.0.0.1";
-    proxyPort = toString config.proxyPort;
-  };
+  jre-with-proxy = let
+    java-with-proxy = pkgs.writeShellScript "java" ''
+      ${pkgs.jre}/bin/java \
+        -Dhttp.proxyHost=127.0.0.1 \
+        -Dhttp.proxyPort=${toString config.proxyPort} \
+        -Dhttps.proxyHost=127.0.0.1 \
+        -Dhttps.proxyPort=${toString config.proxyPort} \
+        "$@"
+    '';
+  in pkgs.runCommand "jre_with_proxy" {} ''
+    mkdir -p $out
+    ${pkgs.xorg.lndir}/bin/lndir -silent ${pkgs.jre} $out
+    rm $out/bin/java
+    ln -s ${java-with-proxy} $out/bin/java
+  '';
 in {
   programs.neovim = {
     extraLuaConfig = /*lua*/ ''
       vim.lsp.enable("metals")
     '';
-    extraPackages = [ (pkgs.metals.override {jre = jre_with_proxy;}) ];
+    extraPackages = [ (pkgs.metals.override {jre = jre-with-proxy;}) ];
   };
 }
