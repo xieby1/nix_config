8cae9b036178402a4bcefabeb1dbb42f3d32e91d
xieby1 <xieby1@outlook.com>
2025-09-20 12:25:32 +0800

    usr: cli: vim: clean image-nvim

--- usr/cli/vim/snacks-nvim.nix
+++ usr/cli/vim/snacks-nvim.nix
@@ -1,3 +1,9 @@
+# image-nvim vs snacks-nvim问题
+# * image-nvim
+#   * svg会变成png，非常不清晰，也没人提问题
+#   * （幻灯片里）图片多了非常卡，会显示不出来
+#   * 排版有问题，总有错位，issues说解决了，我这里用的最新版，仍然部分有问题
+#   * 结论，不如用MarkdownPreview或snacks-nvim/image
 { pkgs, ... }: {
   programs.neovim = {
     plugins = [{
