f169605e6940fd9dfe478c2daae4bc228b024286
xieby1 <xieby1@outlook.com>
2025-09-22 21:41:33 +0800

    usr: cli: vim: close-windows: use lua

--- usr/cli/vim/close-windows.nix
+++ usr/cli/vim/close-windows.nix
@@ -1,53 +1,19 @@
 #MC # key bindings of closing windows
-{ ... }: {
-  programs.neovim = {
-    extraConfig = ''
-      function CloseWindowsAbove()
-        let l:curwin = winnr()
-        while winnr('1k') != l:curwin
-          wincmd k
-          close
-          let l:curwin = winnr()
-        endwhile
-      endfunction
-      function CloseWindowsBelow()
-        let l:curwin = winnr()
-        wincmd j
-        while winnr() != l:curwin
-          close
-        endwhile
-      endfunction
-      function CloseWindowsLeft()
-        let l:curwin = winnr()
-        while winnr('1h') != l:curwin
-          wincmd h
-          close
-          let l:curwin = winnr()
-        endwhile
-      endfunction
-      function CloseWindowsRight()
-        let l:curwin = winnr()
-        wincmd l
-        while winnr() != l:curwin
-          close
-        endwhile
-      endfunction
-
-      command CloseWindowsAbove call CloseWindowsAbove()
-      command CloseWindowsBelow call CloseWindowsBelow()
-      nnoremap Z<Up> :call CloseWindowsAbove()<CR>
-      nnoremap Z<S-Up> :call CloseWindowsAbove()<CR>
-      nnoremap Z<Down> :call CloseWindowsBelow()<CR>
-      nnoremap Z<S-Down> :call CloseWindowsBelow()<CR>
-      command CloseWindowsLeft call CloseWindowsLeft()
-      command CloseWindowsRight call CloseWindowsRight()
-      nnoremap Z<Left> :call CloseWindowsLeft()<CR>
-      nnoremap Z<S-Left> :call CloseWindowsLeft()<CR>
-      nnoremap Z<Right> :call CloseWindowsRight()<CR>
-      nnoremap Z<S-Right> :call CloseWindowsRight()<CR>
-
-      " quit all
-      nnoremap ZA :qa!<CR>
-    '';
-  };
-}
+{ ... }: { programs.neovim.extraLuaConfig = /*lua*/ ''
+  for Dir, cmd in pairs({Left="h", Down="j", Up="k", Right="l"}) do
+    local f = function()
+      local beg = vim.api.nvim_get_current_win()
+      local cur
+      while true do
+        vim.api.nvim_set_current_win(beg)
+        vim.cmd(string.format("wincmd %s", cmd))
+        cur = vim.api.nvim_get_current_win()
+        if cur == beg then break end
+        vim.cmd("q")
+      end
+    end
+    vim.keymap.set('n', string.format("Z<%s>", Dir), f)
+    vim.keymap.set('n', string.format("Z<S-%s>", Dir), f)
+  end
+  vim.keymap.set('n', "ZA", "<CMD>qa!<CR>")
+'';}
