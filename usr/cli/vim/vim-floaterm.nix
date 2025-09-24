5d6b24a0994f506b927d9bf2c479346d3edd6c1e
xieby1 <xieby1@outlook.com>
2025-09-25 00:10:31 +0800

    usr: cli: vim: vim-floaterm: add <leader>T to open terminal in CWD

--- usr/cli/vim/vim-floaterm.nix
+++ usr/cli/vim/vim-floaterm.nix
@@ -4,6 +4,7 @@
     plugin = pkgs.vimPlugins.vim-floaterm;
     config = /*vim*/ ''
       nmap <Leader>t :FloatermNew --cwd=<buffer><CR>
+      nmap <Leader>T :FloatermNew<CR>
       " let g:floaterm_keymap_new = '<Leader>t'
       let g:floaterm_width = 0.8
       let g:floaterm_height = 0.8
