78fb5866605319af8d854e0022bd1cd8e816f83e
xieby1 <xieby1@users.noreply.github.com>
2025-09-30 09:04:27 +0000

    deploy: a0eb2646b8b3b4a254cac91bada4535f6ebfe979

---
+++ usr/gui/fcitx5/default.nix
@@ -0,0 +1,98 @@
+#MC Input method
+#MC
+#MC Why replace ibus with fcitx5
+#MC * ibus mozc not support shift toggle activation
+#MC * ibus configuration use db, not file
+#MC * ibus cannot be configured by user (home-manager)
+{ config, pkgs, ... }: {
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = [
+        pkgs.fcitx5-chinese-addons
+        pkgs.fcitx5-pinyin-zhwiki
+        pkgs.fcitx5-mozc-ut
+        pkgs.fcitx5-hangul
+      ];
+    };
+  };
+
+  imports = [ ./module.nix ];
+  config_fcitx5 = {
+    profile = (pkgs.formats.ini {}).generate "profile" {
+      "Groups/0" = {
+        Name="Default";
+        "Default Layout"="us";
+      };
+      "Groups/0/Items/0" = {
+        Name="keyboard-us";
+      };
+      "Groups/0/Items/1" = {
+        Name="pinyin";
+      };
+      "Groups/0/Items/2" = {
+        Name="mozc";
+      };
+      "Groups/0/Items/3" = {
+        Name="hangul";
+      };
+    };
+    config = (pkgs.formats.ini {}).generate "config" {
+      "Hotkey" = {
+        # Disable default super+space, shift+super+space
+        EnumerateGroupForwardKeys="";
+        EnumerateGroupBackwardKeys="";
+      };
+      "Hotkey/TriggerKeys" = {
+        "0"="Shift_L";
+      };
+      "Hotkey/EnumerateForwardKeys" = {
+        "0"="Control+space";
+      };
+      "Hotkey/EnumerateBackwardKeys" = {
+        "0"="Control+Shift+space";
+      };
+    };
+    "conf/classicui.conf" = (pkgs.formats.keyValue {}).generate "classicui.conf" {
+      Font=''"Sans Serif 16"'';
+      MenuFont=''"Sans Serif 16"'';
+      TrayFont=''"Sans Serif 16"'';
+      Theme="default";
+      DarkTheme="default-dark";
+      # Follow system light/dark color scheme
+      UseDarkTheme="True";
+      # Follow system accent color if it is supported by theme and desktop
+      UseAccentColor="True";
+    };
+    "conf/punctuation.conf" = (pkgs.formats.keyValue {}).generate "punctuation.conf" {
+      # Half width punctuation after latin letter or number
+      HalfWidthPuncAfterLetterOrNumber = "False";
+    };
+    # simplified/traditional chinese conversion
+    "conf/chttrans.conf" = (pkgs.formats.ini {}).generate "chttrans.conf" {
+      Hotkey."0" = "Alt+F";
+    };
+  };
+  home.file.punc_mb_zh_CN = {
+    source = pkgs.runCommand "punc.mb.zh_CN" {} /*bash*/ ''
+      sed '/_ ——/d' ${pkgs.fcitx5-chinese-addons}/share/fcitx5/punctuation/punc.mb.zh_CN > $out
+      {
+      cat <<APPEND
+      < <
+      > >
+      ^ ^
+      APPEND
+      } >> $out
+    '';
+    target = ".local/share/fcitx5/punctuation/punc.mb.zh_CN";
+  };
+
+  dconf.settings = {
+    # Disable ibus input method shortcuts
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-input-source=[];
+      switch-input-source-backward=[];
+    };
+  };
+}
---
+++ usr/gui/firefox/extensions/default.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }: {
+  imports = [
+    ./module.nix
+
+    ./sidebery.nix
+    ./darkreader.nix
+    ./smartproxy.nix
+    ./smart-toc.nix
+    ./brotab.nix
+    ./vimium.nix
+  ];
+
+  programs.firefox = {
+    profiles.xieby1 = {
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        # 😾😾😾 Chinese users cannot use ad block extensions
+        # https://discourse.mozilla.org/t/chinese-users-cant-use-ad-blocker-extensions/94823
+        ublock-origin
+        auto-tab-discard
+        refined-github
+        furiganaize
+      ];
+      settings = {
+        # Automatically enable extensions
+        "extensions.autoDisableScopes" = 0;
+        # Disable extension auto update
+        "extensions.update.enabled" = false;
+      };
+    };
+  };
+}
