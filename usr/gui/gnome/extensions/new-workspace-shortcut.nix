f2a45da521d78c5068f5f597c1c3765c234a9542
xieby1 <xieby1@outlook.com>
2025-11-03 08:48:29 +0800

    usr: gui: gnome: extensions: new-workspace-shortcut: explicitly specify all shortcuts

--- usr/gui/gnome/extensions/new-workspace-shortcut.nix
+++ usr/gui/gnome/extensions/new-workspace-shortcut.nix
@@ -9,8 +9,10 @@
       ];
     };
     "org/gnome/shell/extensions/newworkspaceshortcut" = {
-      empty-workspace-left = [""];
-      empty-workspace-right = [""];
+      empty-workspace-left = ["<Alt><Control><Shift>Left"];
+      empty-workspace-right = ["<Alt><Control><Shift>Right"];
+      move-window-to-left-workspace = ["<Super><Control><Shift>Left"];
+      move-window-to-right-workspace = ["<Super><Control><Shift>Right"];
       workspace-left = [""];
       workspace-right = [""];
     };
