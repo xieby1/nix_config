f81ac4c3c88f125ec67a293d48cc22f145d7e9c9
xieby1 <xieby1@users.noreply.github.com>
2025-12-03 04:15:44 +0000

    deploy: 086f538633b67222a51938be905eb9b42558d717

---
+++ usr/cli/vim/nvim-treesitter-context.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }: {
+  programs.neovim = {
+    plugins = [{
+      plugin = pkgs.vimPlugins.nvim-treesitter-context;
+      type = "lua";
+      config = /*lua*/ ''
+        require'treesitter-context'.setup{}
+        vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey")
+        vim.cmd("hi link TreesitterContext NONE")
+        vim.keymap.set("n", "<leader>c", "<Cmd>TSContextToggle<CR>")
+      '';
+    }];
+  };
+}
