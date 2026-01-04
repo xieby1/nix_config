998c0aaeab2f03bd0ffac2c3ddb5db12e0d76f62
xieby1 <xieby1@outlook.com>
2026-01-04 11:25:42 +0800

    [25.11]: usr: cli: vim: leap-nvim: update config

--- usr/cli/vim/leap-nvim.nix
+++ usr/cli/vim/leap-nvim.nix
@@ -1,16 +1,12 @@
-{ config, pkgs, stdenv, lib, ... }:
-let
-  my-leap-nvim = {
-    plugin = pkgs.vimPlugins.leap-nvim;
-    type = "lua";
-    config = ''
-      require('leap').set_default_mappings()
-    '';
-  };
-in {
+{ pkgs, ... }: {
   programs.neovim = {
-    plugins = [
-      my-leap-nvim
-    ];
+    plugins = [{
+      plugin = pkgs.vimPlugins.leap-nvim;
+      type = "lua";
+      config = /*lua*/ ''
+        vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+        vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+        '';
+    }];
   };
 }
