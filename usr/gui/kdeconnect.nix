13864d01a93316c610cd09a9cc1f0b9ba3a66d7c
xieby1 <xieby1@outlook.com>
2026-01-04 09:52:46 +0800

    [25.11]: usr: gui: kdeconnect

--- usr/gui/kdeconnect.nix
+++ usr/gui/kdeconnect.nix
@@ -1,22 +1,11 @@
 #MC # KDE Connect
-{ config, pkgs, stdenv, lib, ... }:
-let
-  mykdeconnect = pkgs.plasma5Packages.kdeconnect-kde;
-  #MC This customized version of KDE Connect allow share PC's keyboard smoothly to android.
-  #mykdeconnect = pkgs.kdeconnect.overrideAttrs (old: {
-  #  patches = [( pkgs.fetchpatch {
-  #    url = "https://raw.githubusercontent.com/xieby1/kdeconnect-kde-enhanced/4610431b932b2fab05d7e0fc55e7306dc7ff0910/diff.patch";
-  #    hash = "sha256-NL/TVOMEhdJ/W7UTxjF7Qjnq7JciNvl08BC1wrBfvHo=";
-  #  })];
-  #  # cmakeFlags = "-DCMAKE_BUILD_TYPE=Debug -DQT_FORCE_STDERR_LOGGING=1";
-  #});
-in {
+{ pkgs, ... }: {
   home.packages = [
-    mykdeconnect
+    pkgs.kdePackages.kdeconnect-kde
   ];
   #MC Auto startup KDE Connect after Gnome GUI login.
   home.file.kde_connect_indicator = {
-    source = "${mykdeconnect}/share/applications/org.kde.kdeconnect.nonplasma.desktop";
+    source = "${pkgs.kdePackages.kdeconnect-kde}/share/applications/org.kde.kdeconnect.nonplasma.desktop";
     target = ".config/autostart/org.kde.kdeconnect.nonplasma.desktop";
   };
 }
