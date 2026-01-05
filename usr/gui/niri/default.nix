bd6bfb25d11c0b5bf5d95099acb190b6fb7d0396
xieby1 <xieby1@outlook.com>
2026-01-06 01:31:55 +0800

    usr: gui: niri: clean

--- usr/gui/niri/default.nix
+++ usr/gui/niri/default.nix
@@ -91,19 +91,11 @@
         "Mod+Down".focus-window-down = {};
         "Mod+Up".focus-window-up = {};
         "Mod+Right".focus-column-right = {};
-        "Mod+H".focus-column-left = {};
-        "Mod+J".focus-window-down = {};
-        "Mod+K".focus-window-up = {};
-        "Mod+L".focus-column-right = {};
 
         "Mod+Ctrl+Left".move-column-left = {};
         "Mod+Ctrl+Down".move-window-down = {};
         "Mod+Ctrl+Up".move-window-up = {};
         "Mod+Ctrl+Right".move-column-right = {};
-        "Mod+Ctrl+H".move-column-left = {};
-        "Mod+Ctrl+J".move-window-down = {};
-        "Mod+Ctrl+K".move-window-up = {};
-        "Mod+Ctrl+L".move-column-right = {};
 
         "Mod+Home".focus-column-first = {};
         "Mod+End".focus-column-last = {};
@@ -114,33 +106,19 @@
         "Mod+Shift+Down".focus-monitor-down = {};
         "Mod+Shift+Up".focus-monitor-up = {};
         "Mod+Shift+Right".focus-monitor-right = {};
-        "Mod+Shift+H".focus-monitor-left = {};
-        "Mod+Shift+J".focus-monitor-down = {};
-        "Mod+Shift+K".focus-monitor-up = {};
-        "Mod+Shift+L".focus-monitor-right = {};
 
         "Mod+Shift+Ctrl+Left".move-column-to-monitor-left = {};
         "Mod+Shift+Ctrl+Down".move-column-to-monitor-down = {};
         "Mod+Shift+Ctrl+Up".move-column-to-monitor-up = {};
         "Mod+Shift+Ctrl+Right".move-column-to-monitor-right = {};
-        "Mod+Shift+Ctrl+H".move-column-to-monitor-left = {};
-        "Mod+Shift+Ctrl+J".move-column-to-monitor-down = {};
-        "Mod+Shift+Ctrl+K".move-column-to-monitor-up = {};
-        "Mod+Shift+Ctrl+L".move-column-to-monitor-right = {};
 
         "Mod+Page_Down".focus-workspace-down = {};
         "Mod+Page_Up".focus-workspace-up = {};
-        "Mod+U".focus-workspace-down = {};
-        "Mod+I".focus-workspace-up = {};
         "Mod+Ctrl+Page_Down".move-column-to-workspace-down = {};
         "Mod+Ctrl+Page_Up".move-column-to-workspace-up = {};
-        "Mod+Ctrl+U".move-column-to-workspace-down = {};
-        "Mod+Ctrl+I".move-column-to-workspace-up = {};
 
         "Mod+Shift+Page_Down".move-workspace-down = {};
         "Mod+Shift+Page_Up".move-workspace-up = {};
-        "Mod+Shift+U".move-workspace-down = {};
-        "Mod+Shift+I".move-workspace-up = {};
 
         "Mod+WheelScrollDown" = {
           _props.cooldown-ms = 150;
@@ -168,25 +146,6 @@
         "Mod+Shift+WheelScrollUp".focus-column-left = {};
         "Mod+Ctrl+Shift+WheelScrollDown".move-column-right = {};
         "Mod+Ctrl+Shift+WheelScrollUp".move-column-left = {};
-
-        "Mod+1".focus-workspace = 1;
-        "Mod+2".focus-workspace = 2;
-        "Mod+3".focus-workspace = 3;
-        "Mod+4".focus-workspace = 4;
-        "Mod+5".focus-workspace = 5;
-        "Mod+6".focus-workspace = 6;
-        "Mod+7".focus-workspace = 7;
-        "Mod+8".focus-workspace = 8;
-        "Mod+9".focus-workspace = 9;
-        "Mod+Ctrl+1".move-column-to-workspace = 1;
-        "Mod+Ctrl+2".move-column-to-workspace = 2;
-        "Mod+Ctrl+3".move-column-to-workspace = 3;
-        "Mod+Ctrl+4".move-column-to-workspace = 4;
-        "Mod+Ctrl+5".move-column-to-workspace = 5;
-        "Mod+Ctrl+6".move-column-to-workspace = 6;
-        "Mod+Ctrl+7".move-column-to-workspace = 7;
-        "Mod+Ctrl+8".move-column-to-workspace = 8;
-        "Mod+Ctrl+9".move-column-to-workspace = 9;
 
         "Mod+BracketLeft".consume-or-expel-window-left = {};
         "Mod+BracketRight".consume-or-expel-window-right = {};
@@ -226,6 +185,8 @@
         "Ctrl+Alt+Delete".quit = {};
         "Mod+Shift+P".power-off-monitors = {};
       };
+      gestures.hot-corners.off = {};
+      overview.zoom = 0.25;
     };
     target = ".config/niri/config.kdl";
   };
