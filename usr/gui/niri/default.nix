79130adce9f7cbeb709f384d0054b87008ad8ca0
xieby1 <xieby1@outlook.com>
2026-01-10 17:20:23 +0800

    usr: gui: niri: enable only mod bind

--- usr/gui/niri/default.nix
+++ usr/gui/niri/default.nix
@@ -1,6 +1,31 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: let
+  niri = pkgs.niri.overrideAttrs (final: prev: {
+    # Implement release keybinds and modifier-only binds
+    # https://github.com/YaLTeR/niri/pull/2456/commits
+    # src = pkgs.fetchFromGitHub {
+    #   owner = "flowerysong";
+    #   repo = "niri";
+    #   rev = "86edeb3b0b3d1a08d4d4f59705cbc99a732f5e95";
+    #   hash = "sha256-VFOGkBKA03fIXf/BaXsN6CZqkwUTq1gPvTIGrEMmlTQ=";
+    # };
+    src = pkgs.fetchFromGitHub {
+      owner = "flowerysong";
+      repo = "niri";
+      rev = "53447431c4adcfe1572fed5f39ebddc239ca381c";
+      hash = "sha256-onL4kGGpNHYNIaU11hN440RruSJKcTblSi7CwqMbYeM=";
+    };
+    # https://nixos.wiki/wiki/Rust#Using_overrideArgs_with_Rust_Packages
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      src = final.src;
+      hash = "sha256-bh3NrnlFz2m8aCCakgpblFrswh02ByJVPVgxBbTZ6ts=";
+    };
+    # Unnecessary due to cargoDeps having higher priority than cargoHash,
+    # but to make it explicitly that cargoHash is not used after overrideAttrs.
+    cargoHash = null;
+  });
+in {
   home.packages = [
-    pkgs.niri
+    niri
   ];
   home.file.niri_config = {
     text = config.lib.generators.toKDL {} {
@@ -79,8 +104,8 @@
         # Open/close the Overview: a zoomed-out view of workspaces and windows.
         # You can also move the mouse into the top-left hot corner,
         # or do a four-finger swipe up on a touchpad.
-        "Mod+O" = {
-          _props.repeat = false;
+        "Mod" = {
+          _props = { repeat = false; release = true; };
           toggle-overview = {};
         };
 
