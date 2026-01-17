a8ad664baa3725fb82f688ad7e2ee3cc9a823188
xieby1 <xieby1@outlook.com>
2026-01-17 23:16:46 +0800

    usr: gui: gnome: extensions: hide-top-bar: fix

--- usr/gui/gnome/extensions/hide-top-bar.nix
+++ usr/gui/gnome/extensions/hide-top-bar.nix
@@ -2,11 +2,17 @@
 { pkgs, ... }: {
   home.packages = [
     pkgs.gnomeExtensions.hide-top-bar
+    # https://extensions.gnome.org/extension/545/hide-top-bar/
+    # > straydog77:
+    # > If anyone is having issues with this extension behaving strangely like the top bar sometimes won't appear or appear briefly and disappear use the "disable-unredirect" extension in conjunction
+    # > https://extensions.gnome.org/extension/8008/disable-unredirect/
+    pkgs.gnomeExtensions.disable-unredirect
   ];
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
         "hidetopbar@mathieu.bidon.ca"
+        "disable-unredirect@exeos"
       ];
     };
     "org/gnome/shell/extensions/hidetopbar" = {
