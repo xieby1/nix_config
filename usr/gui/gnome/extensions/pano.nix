1e542cdc6150012f545bbfd4abbb6c0540a532b7
xieby1 <xieby1@outlook.com>
2026-01-17 23:41:36 +0800

    usr: gui: gnome: extensions: pano: fix

--- usr/gui/gnome/extensions/pano.nix
+++ usr/gui/gnome/extensions/pano.nix
@@ -1,7 +1,14 @@
 #MC # pano: Next-gen Clipboard Manager for Gnome Shell
 { pkgs, ... }: {
   home.packages = [
-    pkgs.gnomeExtensions.pano
+    (pkgs.gnomeExtensions.pano.overrideAttrs (old: {
+      # Referring to https://github.com/NixOS/nixpkgs/pull/461745/files
+      # prepend_search_path => dup_default().prepend_search_path
+      preInstall = builtins.replaceStrings
+        ["prepend_search_path"]
+        ["dup_default().prepend_search_path"]
+        old.preInstall;
+    }))
   ];
   dconf.settings = {
     "org/gnome/shell" = {
