8c6f97f04e8963f3cd0e19edb529cd8af36f9b73
xieby1 <xieby1@outlook.com>
2026-02-01 11:37:53 +0800

    usr: gui: dms: settings: custome bar right

--- usr/gui/dms/settings.nix
+++ usr/gui/dms/settings.nix
@@ -7,6 +7,16 @@
       autoHide = true;
       showOnWindowsOpen = true;
       openOnOverview = true;
+      rightWidgets = [
+        { enabled = true; id = "systemTray"; }
+        { enabled = true; id = "cpuUsage"; }
+        { enabled = true; id = "memUsage"; showSwap = true; }
+        { enabled = true; id = "diskUsage"; }
+        { enabled = true; id = "clipboard"; }
+        { enabled = true; id = "notificationButton"; }
+        { enabled = true; id = "battery"; }
+        { enabled = true; id = "controlCenterButton"; }
+      ];
     }];
   };
 }
