4e03ecc4a6da0f10fc5e064ebdcbf392644f7ccf
xieby1 <xieby1@outlook.com>
2026-02-03 22:50:26 +0800

    npins: nur, pkgsu, flake-compat

--- config.nix
+++ config.nix
@@ -15,16 +15,11 @@
   allowUnfree = true;
   packageOverrides = pkgs: rec {
     #MC 添加nix user repository (NUR)到nixpkgs里。
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-      pkgs = pkgsu;
-    };
+    nur = import (import ./npins).nur { pkgs = pkgsu; };
     #MC 添加非稳定版的nixpkgs到nixpkgs里，
     #MC 比如非稳定版的hello可以通过`pkgs.pkgsu.hello`来访问。
-    pkgsu = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
+    pkgsu = import (import ./npins).pkgsu {};
     #MC 添加flake-compat，用于在nix expression中使用flake的包
-    flake-compat = import (builtins.fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/0f9255e01c2351cc7d116c072cb317785dd33b33.tar.gz";
-      sha256 = "0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
-    });
+    flake-compat = import (import ./npins).flake-compat;
   };
 }
