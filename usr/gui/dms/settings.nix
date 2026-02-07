51cd66f8ef0a996a584f78fc42611a7ad134670a
xieby1 <xieby1@outlook.com>
2026-02-07 19:12:36 +0800

    usr: gui: dms: add default-settings

--- usr/gui/dms/settings.nix
+++ usr/gui/dms/settings.nix
@@ -1,22 +1,31 @@
-{ ... }: {
-  yq-merge.".config/DankMaterialShell/settings.json".text = builtins.toJSON {
-    showDock = true;
-    dockAutoHide = true;
-    dockOpenOnOverview = true;
-    barConfigs = [{
-      autoHide = true;
-      showOnWindowsOpen = true;
-      openOnOverview = true;
-      rightWidgets = [
-        { enabled = true; id = "systemTray"; }
-        { enabled = true; id = "cpuUsage"; }
-        { enabled = true; id = "memUsage"; showSwap = true; }
-        { enabled = true; id = "diskUsage"; }
-        { enabled = true; id = "clipboard"; }
-        { enabled = true; id = "notificationButton"; }
-        { enabled = true; id = "battery"; }
-        { enabled = true; id = "controlCenterButton"; }
-      ];
-    }];
+{ ... }: let
+  settings-relpath = ".config/DankMaterialShell/settings.json";
+in {
+  yq-merge.${settings-relpath} = {
+    preOnChange = ''
+      if [[ ! -e ~/${settings-relpath} ]]; then
+        cat ${import ./default-settings.nix} > ~/${settings-relpath}
+      fi
+    '';
+    text = builtins.toJSON {
+      showDock = true;
+      dockAutoHide = true;
+      dockOpenOnOverview = true;
+      barConfigs = [{
+        autoHide = true;
+        showOnWindowsOpen = true;
+        openOnOverview = true;
+        rightWidgets = [
+          { enabled = true; id = "systemTray"; }
+          { enabled = true; id = "cpuUsage"; }
+          { enabled = true; id = "memUsage"; showSwap = true; }
+          { enabled = true; id = "diskUsage"; }
+          { enabled = true; id = "clipboard"; }
+          { enabled = true; id = "notificationButton"; }
+          { enabled = true; id = "battery"; }
+          { enabled = true; id = "controlCenterButton"; }
+        ];
+      }];
+    };
   };
 }
