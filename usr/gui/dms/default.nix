9345b235295bc0f6fb6288a47b892a51ac1642b2
xieby1 <xieby1@outlook.com>
2026-02-07 17:55:02 +0800

    usr: gui: dms: use npins

--- usr/gui/dms/default.nix
+++ usr/gui/dms/default.nix
@@ -4,12 +4,7 @@
       # TODO: Why does `pkgs` above not work?
       pkgs = import <nixpkgs> {};
       flake-dms = pkgs.flake-compat {
-        src = pkgs.fetchFromGitHub {
-          owner = "AvengeMedia";
-          repo = "DankMaterialShell";
-          rev = "v1.2.3";
-          hash = "sha256-P//moH3z9r4PXirTzXVsccQINsK5AIlF9RWOBwK3vLc=";
-        };
+        src = (pkgs.npinsed {input = ./npins.json;}).DankMaterialShell;
       };
     in flake-dms.defaultNix.homeModules.dank-material-shell)
     ./settings.nix
@@ -19,12 +14,7 @@
     # https://github.com/AvengeMedia/DankMaterialShell/issues/1489
     dgop.package = let
       flake-dgop = pkgs.flake-compat {
-        src = pkgs.fetchFromGitHub {
-          owner = "AvengeMedia";
-          repo = "dgop";
-          rev = "3cd297080573319c36884b28a3cc0dbfed79f53e";
-          hash = "sha256-NBGF7bo+nYSToDWtIXMwPeulzRxphX8JHzNRqLTOOqU=";
-        };
+        src = (pkgs.npinsed {input = ./npins.json;}).dgop;
       };
     in flake-dgop.defaultNix.packages.${builtins.currentSystem}.default;
   };
