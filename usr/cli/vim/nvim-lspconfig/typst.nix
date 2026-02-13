503de92046c3aecccbc6064390cb83f7d17f852b
xieby1 <xieby1@outlook.com>
2026-02-13 12:15:43 +0800

    usr: cli: vim: nvim-lspconfig: typst: preview open in evince

--- usr/cli/vim/nvim-lspconfig/typst.nix
+++ usr/cli/vim/nvim-lspconfig/typst.nix
@@ -15,6 +15,13 @@
         if vim.lsp.get_clients({name="tinymist"})[1].settings.exportPdf ~= "onSave" then
           -- more settings see https://github.com/Myriad-Dreamin/tinymist/editors/neovim/Configuration.md
           vim.lsp.config("tinymist", {settings = {exportPdf = "onSave"}})
+          -- compile and open pdf in evince
+          local file_typ = vim.fn.expand('%:p')
+          local file_pdf = vim.fn.expand('%:p:r') .. '.pdf'
+          vim.system({'bash', '-c',
+            'typst compile ' .. file_typ .. ' ' .. file_pdf .. ';' ..
+            'evince ' .. file_pdf
+          }, { text = true }, function()end)
           print("exportPdf = onSave")
         else
           vim.lsp.config("tinymist", {settings = {exportPdf = "never"}})
