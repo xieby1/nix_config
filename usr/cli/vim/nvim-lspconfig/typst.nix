26d54eaf241837ec20ce8ca0c533f50a7efce64c
xieby1 <xieby1@outlook.com>
2026-02-28 00:54:12 +0800

    usr: cli: vim: nvim-lspconfig: typst: allow treesitter injections

--- usr/cli/vim/nvim-lspconfig/typst.nix
+++ usr/cli/vim/nvim-lspconfig/typst.nix
@@ -10,6 +10,19 @@
       '';
     }];
     extraLuaConfig = /*lua*/ ''
+      -- https://www.reddit.com/r/neovim/comments/1iiqwb1/treesitter_injection_and_priority_issues/
+      -- The `@lsp.type.comment.typst` can be obtained by hovering on the text, then `:Inspect`
+      -- TLDR: I want to use typst injections.scm,
+      --       while treesitter default highlight priority is 100,
+      --       typst lsp highlight priority is 125.
+      -- My failed attempts:
+      -- * (#set! priority ): it only works in highlights.scm, does not work in injections.scm
+      --   * See: https://github.com/nvim-treesitter/nvim-treesitter/issues/3110
+      -- * Turn of tinymist lsp highlight:
+      --   * `semanticTokens = "disable"` does not turn off tinymist lsp syntax highlight.
+      -- So current my approach is keeping tinymist lsp highlight,
+      -- only clear @lsp.type.comment.typst group.
+      vim.api.nvim_set_hl(0, "@lsp.type.comment.typst", {})
       vim.lsp.enable('tinymist')
       vim.api.nvim_create_user_command("TypstExportPdfOnSaveToggle", function(args)
         if vim.lsp.get_clients({name="tinymist"})[1].settings.exportPdf ~= "onSave" then
