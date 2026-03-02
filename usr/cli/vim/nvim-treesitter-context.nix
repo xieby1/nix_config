a4f8604b898200b497342d08d0f460794b0aa436
xieby1 <xieby1@outlook.com>
2026-03-02 22:17:29 +0800

    usr: cli: vim: nvim-treesitter-context: multiwindow

--- usr/cli/vim/nvim-treesitter-context.nix
+++ usr/cli/vim/nvim-treesitter-context.nix
@@ -4,7 +4,10 @@
       plugin = pkgs.vimPlugins.nvim-treesitter-context;
       type = "lua";
       config = /*lua*/ ''
-        require'treesitter-context'.setup({enable = false})
+        require'treesitter-context'.setup({
+          enable = false,
+          multiwindow = true,
+        })
         vim.cmd("hi TreesitterContextBottom gui=underline guisp=Grey")
         vim.cmd("hi link TreesitterContext NONE")
         vim.keymap.set("n", "<leader>c", "<Cmd>TSContext toggle<CR>")
