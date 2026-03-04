13740d9babf66497c8b03153ec3b3f857e232e12
xieby1 <xieby1@outlook.com>
2026-03-04 22:20:13 +0800

    usr: cli: vim: nvim-lspconfig: typst: TypstExportPdfOnSaveToggle => TypstExportPdfOnSaveEnable and TypstExportPdfOnSaveXDisable

--- usr/cli/vim/nvim-lspconfig/typst.nix
+++ usr/cli/vim/nvim-lspconfig/typst.nix
@@ -24,22 +24,26 @@
       -- only clear @lsp.type.comment.typst group.
       vim.api.nvim_set_hl(0, "@lsp.type.comment.typst", {})
       vim.lsp.enable('tinymist')
-      vim.api.nvim_create_user_command("TypstExportPdfOnSaveToggle", function(args)
-        if vim.lsp.get_clients({name="tinymist"})[1].settings.exportPdf ~= "onSave" then
-          -- more settings see https://github.com/Myriad-Dreamin/tinymist/editors/neovim/Configuration.md
-          vim.lsp.config("tinymist", {settings = {exportPdf = "onSave"}})
-          -- compile and open pdf in evince
-          local file_typ = vim.fn.expand('%:p')
-          local file_pdf = vim.fn.expand('%:p:r') .. '.pdf'
-          vim.system({'bash', '-c',
-            'typst compile ' .. file_typ .. ' ' .. file_pdf .. ';' ..
-            'evince ' .. file_pdf
-          }, { text = true }, function()end)
-          print("exportPdf = onSave")
-        else
-          vim.lsp.config("tinymist", {settings = {exportPdf = "never"}})
-          print("exportPdf = never")
-        end
+      vim.api.nvim_create_user_command("TypstExportPdfOnSaveEnable", function(args)
+        -- more settings see https://github.com/Myriad-Dreamin/tinymist/editors/neovim/Configuration.md
+        vim.lsp.config("tinymist", {settings = {exportPdf = "onSave"}})
+        -- compile and open pdf in evince
+        local file_typ = vim.fn.expand('%:p')
+        local file_pdf = vim.fn.expand('%:p:r') .. '.pdf'
+        vim.system({'bash', '-c',
+          'typst compile ' .. file_typ .. ' ' .. file_pdf .. ';' ..
+          'evince ' .. file_pdf
+        }, { text = true }, function()end)
+        print("exportPdf = onSave")
+        -- restart tinymist lsp, see `:h vim.lsp.enable`
+        vim.lsp.enable("tinymist", false)
+        vim.lsp.enable("tinymist", true)
+      end, {})
+      -- Why called TypstExportPdfOnSaveXDisable instead of TypstExportPdfOnSaveDisable:
+      -- to make TypstExportPdfOnSaveXDisable rank after TypstExportPdfOnSaveEnable
+      vim.api.nvim_create_user_command("TypstExportPdfOnSaveXDisable", function(args)
+        vim.lsp.config("tinymist", {settings = {exportPdf = "never"}})
+        print("exportPdf = never")
         -- restart tinymist lsp, see `:h vim.lsp.enable`
         vim.lsp.enable("tinymist", false)
         vim.lsp.enable("tinymist", true)
