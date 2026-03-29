27892d029a19f5f603c973aa231c863b7714e07b
xieby1 <xieby1@outlook.com>
2026-03-30 01:29:31 +0800

    usr: cli: crush: no think

--- usr/cli/crush.nix
+++ usr/cli/crush.nix
@@ -12,6 +12,10 @@
       siliconflow = {
         type = "openai-compat";
         inherit (config.ai.siliconflow) base_url api_key;
+        extra_body = {
+          # TODO: Why the think is enable by default for customized providers?
+          enable_thinking = false;
+        };
         models = [
           config.ai.siliconflow.models.deepseek
         ];
