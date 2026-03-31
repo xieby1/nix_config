927b4aefcdf08404a849793552bad9300059924f
xieby1 <xieby1@outlook.com>
2026-03-31 15:03:25 +0800

    usr: cli: crush: add comparisons crush and aider

--- usr/cli/crush.nix
+++ usr/cli/crush.nix
@@ -1,12 +1,42 @@
-# Alternatives Comparisons
-# - Opencode
-#   - Cons:
-#     - Lack of doc, currently I care about customized providers.
-#     - Does not compatible with kitty scrollback.
-#     - Use Esc instead of Ctrl-C to interrupt.
-#     - The input line does not support Home, End key.
-#   - Summary: Its quality is not comparable to neovim,
-#              though it declares "OpenCode is built by neovim users ...".
+# # Alternatives Comparisons
+#
+# ## Opencode
+#
+# - Pros:
+#   - Javascript based. The nix closure is not so large: 186MB!
+# - Cons:
+#   - Lack of doc, currently I care about customized providers.
+#   - Does not support terminal scrollback.
+#   - Use Esc instead of Ctrl-C to interrupt.
+#   - ~~The input line does not support Home, End key.~~
+#   - Token inefficiency
+# - Summary: Its quality is not comparable to neovim,
+#            though it declares "OpenCode is built by neovim users ...".
+#
+# ## Crush
+#
+# - Pros:
+#   - Go lang based.
+# - Cons:
+#   - Token inefficiency: 300 line system prompt and cannot fully customize,
+#                         only supports system prompt prefix.
+#   - The input line support Home, End key, but does not support Ctrl+Left/Right.
+#   - Does not support terminal scrollback.
+#   - Notification cannot click and jump to the corresponding terminal.
+#
+# ## Aider
+#
+# - Pros:
+#   - Support script
+#   - Token efficiency
+#     - Test Prompt: Introduce yourself
+#       - Aider: 2.3k
+#       - Crush: 15K
+#   - Native input line support: scrollback, Ctrl-Left/Right, ...
+# - Cons:
+#   - Python based, large codebase: nix closure > 2GB
+#   - Does not support skill, memory.
+#   - Models are out of date, e.g.: minimax 2.5, while latest is 2.7
 { pkgs, config, ... }: {
   home.packages = [
     (import pkgs.npinsed.nur-charmbracelet {}).crush
