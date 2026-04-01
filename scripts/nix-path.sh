579043e2ea04e847347e9e5bd9b2b83615a030c9
xieby1 <xieby1@outlook.com>
2026-04-01 04:46:54 +0000

    npins: support nix-on-droid

--- scripts/nix-path.sh
+++ scripts/nix-path.sh
@@ -1,9 +1,20 @@
-if [[ ! -d ~/.config/npins || ~/.config/timestamp -ot ~/.config/nixpkgs/npins/sources.json ]]; then
-  NIXPKGS=$(nix eval --impure --expr '(import ~/.config/nixpkgs/npins).nixpkgs.outPath' | tr -d '"')
-  HOME_MANAGER=$(nix eval --impure --expr '(import ~/.config/nixpkgs/npins).home-manager.outPath' | tr -d '"')
+if [[ ! -d ~/.config/npins || ~/.config/npins-timestamp -ot ~/.config/nixpkgs/npins/sources.json ]]; then
   mkdir -p ~/.config/npins
-  nix-store --add-root ~/.config/npins/nixpkgs      --realise $NIXPKGS
-  nix-store --add-root ~/.config/npins/home-manager --realise $HOME_MANAGER
-  touch ~/.config/timestamp
+  nix-store --add-root ~/.config/npins/nixpkgs      \
+    --realise $(nix --extra-experimental-features nix-command eval --impure --expr '(import ~/.config/nixpkgs/npins).nixpkgs.outPath' | tr -d '"')
+  nix-store --add-root ~/.config/npins/home-manager \
+    --realise $(nix --extra-experimental-features nix-command eval --impure --expr '(import ~/.config/nixpkgs/npins).home-manager.outPath' | tr -d '"')
+  if [[ $USER == "nix-on-droid" ]]; then
+    nix-store --add-root ~/.config/npins/nix-on-droid \
+      --realise $(nix --extra-experimental-features nix-command eval --impure --expr '(import ~/.config/nixpkgs/npins).nix-on-droid.outPath' | tr -d '"')
+  fi
+  touch ~/.config/npins-timestamp
 fi
-export NIX_PATH="nixpkgs=$(realpath ~/.config/npins/nixpkgs):home-manager=$(realpath ~/.config/npins/home-manager):nixos-config=/etc/nixos/configuration.nix"
+
+NIX_PATH="nixpkgs=$(realpath ~/.config/npins/nixpkgs):home-manager=$(realpath ~/.config/npins/home-manager)"
+if [[ $USER == "nix-on-droid" ]]; then
+  NIX_PATH=$NIX_PATH:nix-on-droid=$(realpath ~/.config/npins/nix-on-droid)
+else
+  NIX_PATH=$NIX_PATH:nixos-config=/etc/nixos/configuration.nix
+fi
+export NIX_PATH
