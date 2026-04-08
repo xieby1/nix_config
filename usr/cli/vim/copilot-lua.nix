66aae2ddd2939a18741f73fdbf1a034bc85df59f
xieby1 <xieby1@outlook.com>
2026-04-08 14:38:46 +0800

    usr: cli: vim: copilot-lua: TODO

--- usr/cli/vim/copilot-lua.nix
+++ usr/cli/vim/copilot-lua.nix
@@ -1,4 +1,5 @@
 # TODO: why claude is not available?
+# TODO: it seems every nvim instances start a node, which consumes tons of memory (900MB per node), use lazy.nvim!
 { pkgs, ... }: {
   programs.neovim.plugins = [{
     plugin = pkgs.pkgsu.vimPlugins.copilot-lua;
