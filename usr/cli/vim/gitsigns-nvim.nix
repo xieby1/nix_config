438753bc6902cca01783f424c2de4370cac50371
xieby1 <xieby1@outlook.com>
2026-04-11 23:37:03 +0800

    feat(gitsigns): add ]C and [C for navigating all hunks

--- usr/cli/vim/gitsigns-nvim.nix
+++ usr/cli/vim/gitsigns-nvim.nix
@@ -29,9 +29,21 @@ let
             return '<Ignore>'
           end, {expr=true})
 
+          map('n', ']C', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk({target="all"}) end)
+            return '<Ignore>'
+          end, {expr=true})
+
           map('n', '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr=true})
+
+          map('n', '[C', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk({target="all"}) end)
             return '<Ignore>'
           end, {expr=true})
 
