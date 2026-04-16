474583728b0754b9475a68ee3c448c88ce6d3913
xieby1 <xieby1@outlook.com>
2026-04-16 14:41:55 +0800

    refactor(forge): use npins

--- usr/cli/ai/forge.nix
+++ usr/cli/ai/forge.nix
@@ -1,11 +1,12 @@
 { pkgs, ... }: let
-  forgecode = (pkgs.flake-compat {src = pkgs.npinsed.forgecode;}).defaultNix.default
+  npinsed-forgecode = (pkgs.npinsed {input=./npins/sources.json;}).forgecode;
+  forgecode = (pkgs.flake-compat {src = npinsed-forgecode;}).defaultNix.default
     # The version and APP_VERSION is 0.1.0-dev, which is out-of-date
     .overrideAttrs (_old: rec {
       # Forge skips update checks when version contains "dev", avoiding prompts like:
       # `Confirm upgrade from v2.9.9 -> v2.10.0 (latest)? Y/n:`
       # See: crates/forge_main/src/update.rs
-      version = "${pkgs.npinsed.forgecode.version}-dev";
+      version = "${npinsed-forgecode.version}-dev";
       APP_VERSION=version;
       __intentionallyOverridingVersion=true;
     });
