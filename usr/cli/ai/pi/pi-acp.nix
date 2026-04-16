d913be8aba65baf586790cf91f1156974d7e180e
xieby1 <xieby1@outlook.com>
2026-04-16 21:49:20 +0800

    fix(ai/pi-acp): bypass hasAnyPiAuthConfigured for hardcoded auth keys

--- usr/cli/ai/pi/pi-acp.nix
+++ usr/cli/ai/pi/pi-acp.nix
@@ -5,6 +5,13 @@
 buildNpmPackage (finalAttrs: {
   name = "pi-acp";
   src = (npinsed {input=../npins/sources.json;}).pi-acp;
+  # pi-acp attempts to verify whether pi has authenticated to any models.
+  # In my environment, none of the pi-acp check methods work,
+  # because I hard-code the auth key in pi extensions.
+  # Therefore, hasAnyPiAuthConfigured is made to return true directly.
+  postPatch = ''
+    sed -i '/function hasAnyPiAuthConfigured/a\  return true' src/pi-auth/status.ts
+  '';
   npmDepsHash = "sha256-GuHvjqSD4M87cGBtFFSF37FWF79+6pLlai0A99Ii/hM=";
   npmRebuildFlags = [ "--ignore-scripts" ];
 })
