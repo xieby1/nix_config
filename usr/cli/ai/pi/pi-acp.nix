21c6bffc46aaed764cf7482953668982bb76db81
xieby1 <xieby1@outlook.com>
2026-04-16 14:43:00 +0800

    chore(pi-acp)

--- usr/cli/ai/pi/pi-acp.nix
+++ usr/cli/ai/pi/pi-acp.nix
@@ -6,5 +6,6 @@ buildNpmPackage (finalAttrs: {
   name = "pi-acp";
   src = (npinsed {input=../npins/sources.json;}).pi-acp;
   npmDepsHash = "sha256-GuHvjqSD4M87cGBtFFSF37FWF79+6pLlai0A99Ii/hM=";
+  npmRebuildFlags = [ "--ignore-scripts" ];
 })
 
