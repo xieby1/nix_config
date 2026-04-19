3b0603cef0162b6eb844c5b6ac826c71c5c26695
xieby1 <xieby1@outlook.com>
2026-04-19 22:38:17 +0800

    fix(dms): creat .config/DankMaterialShell/plugin_settings.json if does not exist

--- usr/gui/dms/plugins/default.nix
+++ usr/gui/dms/plugins/default.nix
@@ -3,4 +3,11 @@
     ./bing-wallpaper.nix
     ./unified-taskbar
   ];
+  yq-merge.".config/DankMaterialShell/plugin_settings.json" = {
+    preOnChange = ''
+      if [[ ! -e ~/.config/DankMaterialShell/plugin_settings.json ]]; then
+        echo '{}' > ~/.config/DankMaterialShell/plugin_settings.json
+      fi
+    '';
+  };
 }
