c027454db1008e90a1e511343d915f197e052dbe
xieby1 <xieby1@outlook.com>
2026-04-21 20:35:24 +0800

    docs(pi/extensions): deprecate several extensions and list reasons

--- usr/cli/ai/pi/extensions/default.nix
+++ usr/cli/ai/pi/extensions/default.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
   home.file = {
+    # - pi-skill-evolution: needs pi-session-search which needs embedding provider (llm support embedding)
+    # - pi-memory does not work: hard to trigger, too many slash commands
+    # - taskplane: too complex, and subagent call does not work due to `npm root -g` cannot find pi, why not use $PATH?
+    # - pi-messenger: communication cost too expensive
     pi-subagents = {
       target = ".pi/agent/extensions/pi-subagents";
       source = pkgs.npinsed.ai.pi-subagents;
@@ -14,10 +18,6 @@
         npmDepsHash = "sha256-zau3eaJoa8pE3A5COXwyTLSesoePgYqrnRCg3SMSarw=";
         dontNpmBuild = true;
       }) + /lib/node_modules/pi-web-access;
-    };
-    pi-skill-evolution = {
-      target = ".pi/agent/extensions/pi-skill-evolution";
-      source = pkgs.npinsed.ai.pi-skill-evolution;
     };
   };
 }
