8ec44c98f476edf5d8ba0daaec41e0a43482228b
xieby1 <xieby1@outlook.com>
2026-04-27 14:03:54 +0800

    feat(lspconfig): lua-ls => emmylua-ls

--- usr/cli/vim/nvim-lspconfig/default.nix
+++ usr/cli/vim/nvim-lspconfig/default.nix
@@ -7,7 +7,15 @@
     # html
     {programs.neovim={extraLuaConfig="vim.lsp.enable('html')\n";extraPackages=[pkgs.vscode-langservers-extracted];};}
     # lua
-    {programs.neovim={extraLuaConfig="vim.lsp.enable('lua_ls')\n";extraPackages=[pkgs.lua-language-server];};}
+    # TODO: The symlink support has been added to upstream:
+    #       https://github.com/EmmyLuaLs/emmylua-analyzer-rust/commit/b087ffe4f47cdc2c48c99da16b33745dbae1a587
+    #       Remove the following patch once nixpkgs stable include it.
+    {programs.neovim={extraLuaConfig="vim.lsp.enable('emmylua_ls')\n";extraPackages=[(pkgs.emmylua-ls.overrideAttrs(old: {
+      postPatch = (old.postPatch or "") + ''
+        grep 'follow_links(true)' crates/emmylua_code_analysis/src/vfs/loader.rs && exit
+        sed -i '/WalkDir::new(root)/a\.follow_links(true)' crates/emmylua_code_analysis/src/vfs/loader.rs
+      '';
+    }))];};}
     # nix
     {programs.neovim={extraLuaConfig="vim.lsp.enable('nixd')\n";extraPackages=[pkgs.nixd];};}
     # python
