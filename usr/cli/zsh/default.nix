5ee03bad86a551ba8c29fc59cb43123b7dca92c2
xieby1 <xieby1@outlook.com>
2026-05-04 19:25:17 +0800

    feat(zsh): vi-mode

--- usr/cli/zsh/default.nix
+++ usr/cli/zsh/default.nix
@@ -1,5 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.zsh = {
     enable = true;
+    plugins = [{
+      name = "vi-mode";
+      src = pkgs.zsh-vi-mode;
+      file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+    }];
   };
 }
