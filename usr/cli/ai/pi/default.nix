7fef9601724648b1797bda3e22a4c1a7cdfb6905
xieby1 <xieby1@outlook.com>
2026-05-07 00:22:47 +0800

    chore(ai): use pi from pkgsu, clean

--- usr/cli/ai/pi/default.nix
+++ usr/cli/ai/pi/default.nix
@@ -1,23 +1,9 @@
-{ pkgs, config, lib, ... }: let
-  pi-coding-agent = pkgs.pkgsu.pi-coding-agent.overrideAttrs (final: prev: {
-    # pi-coding-agent will check the version in check phase. The prefix "v" should be removed.
-    # E.g.: "v0.67.3" => "0.67.3"
-    version = pkgs.lib.removePrefix "v" final.src.version;
-    src = pkgs.npinsed.ai.pi-mono;
-    npmDepsHash = "sha256-3xFxY0iKiwjM0psijzdSqed5UOjIAOyWPwQ15fqfc4I=";
-    npmDeps = pkgs.fetchNpmDeps {
-      src = final.src;
-      name = "pi-mono-${final.version}-npm-deps";
-      hash = final.npmDepsHash;
-    };
-  });
-in {
+{ pkgs, config, lib, ... }: {
   imports = [ ./extensions ];
   home.packages = [
-    pi-coding-agent
+    pkgs.pkgsu.pi-coding-agent
     (pkgs.callPackage ./pi-acp.nix {})
   ];
-  cachix_packages = [pi-coding-agent];
   home.file = let
     gen-home-file-entry = args: {
       name = "pi-${args.catwalk-provider.id}";
