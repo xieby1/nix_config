ff1debfead8c982dbd39d62b1adf9fb7c2ee4677
xieby1 <xieby1@outlook.com>
2026-05-06 14:43:13 +0800

    fix(forge): zsh color

--- usr/cli/ai/forge/default.nix
+++ usr/cli/ai/forge/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: let
+{ pkgs, config, lib, ... }: let
   forgecode = (pkgs.flake-compat {src = pkgs.npinsed.ai.forgecode;}).defaultNix.default
     # The version and APP_VERSION is 0.1.0-dev, which is out-of-date
     .overrideAttrs (_old: rec {
@@ -44,7 +44,7 @@ in {
       };
     };
   };
-  programs.zsh.initContent = ''
+  programs.zsh.initContent = lib.mkAfter ''
     . ${pkgs.npinsed.ai.forgecode}/shell-plugin/forge.setup.zsh
   '';
 }
