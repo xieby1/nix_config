5836de08e28c320f0cb5e1987bafe49f98d7bf33
xieby1 <xieby1@outlook.com>
2026-05-13 22:16:48 +0800

    chore(ai: hermes): move hermes-agent to let...in block

--- usr/cli/ai/hermes/default.nix
+++ usr/cli/ai/hermes/default.nix
@@ -1,11 +1,13 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, config, ... }: let
+  # llm-agents.hermes-agent vs hermes-agent/flake.nix
+  # - llm-agents.hermes-agent non-cached closure size: 278,345,760B
+  # - hermes-agent/flake.nix non-cached closure size: 1,336,474,112B
+  # Thus llm-agents.hermes-agent is preferred
+  hermes-agent = (pkgs.flake-compat {src=pkgs.npinsed.ai.llm-agents;})
+    .defaultNix.packages.${pkgs.stdenv.system}.hermes-agent;
+in {
   home.packages = [
-    # llm-agents.hermes-agent vs hermes-agent/flake.nix
-    # - llm-agents.hermes-agent non-cached closure size: 278,345,760B
-    # - hermes-agent/flake.nix non-cached closure size: 1,336,474,112B
-    # Thus llm-agents.hermes-agent is preferred
-    (pkgs.flake-compat {src=pkgs.npinsed.ai.llm-agents;})
-      .defaultNix.packages.${pkgs.stdenv.system}.hermes-agent
+    hermes-agent
   ];
   yq-merge.".hermes/config.yaml" = {
     generator = builtins.toJSON;
