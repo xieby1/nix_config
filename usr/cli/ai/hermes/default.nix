8c5b6a8798042580788610ca97f39c896608f593
xieby1 <xieby1@outlook.com>
2026-05-13 22:08:53 +0800

    feat(ai: hermes): config

--- usr/cli/ai/hermes/default.nix
+++ usr/cli/ai/hermes/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, config, ... }: {
   home.packages = [
     # llm-agents.hermes-agent vs hermes-agent/flake.nix
     # - llm-agents.hermes-agent non-cached closure size: 278,345,760B
@@ -7,4 +7,22 @@
     (pkgs.flake-compat {src=pkgs.npinsed.ai.llm-agents;})
       .defaultNix.packages.${pkgs.stdenv.system}.hermes-agent
   ];
+  yq-merge.".hermes/config.yaml" = {
+    generator = builtins.toJSON;
+    expr = {
+      model = {
+        default = "kimi-for-coding";
+        provider = "kimi-coding";
+        base_url = config.ai.kimi.api_endpoint;
+      };
+    };
+  };
+  yq-merge.".hermes/.env" = {
+    generator = lib.generators.toKeyValue {};
+    yqExtraArgs = "-p=props -o=props --properties-separator='='";
+    yqLoadFunc = "load_props";
+    expr = {
+      KIMI_API_KEY = config.ai.kimi.api_key;
+    };
+  };
 }
