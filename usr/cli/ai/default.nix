92c49795505554a25a3a9ed5abe93eb215e2e1f8
xieby1 <xieby1@outlook.com>
2026-05-13 11:36:03 +0800

    feat(ai): add goose

--- usr/cli/ai/default.nix
+++ usr/cli/ai/default.nix
@@ -7,8 +7,11 @@
     ./mcp/vestige
   ];
   home.packages = [
+    # TODO: use latest nixpkgs and remove nixpkgs-for-copilot and nixpkgs-goose-cli
     # copilot error: Invalid shell ID: 0. Please supply a valid shell ID to read output from.
     # https://github.com/NixOS/nixpkgs/pull/509133
     (import pkgs.npinsed.ai.nixpkgs-for-copilot {}).github-copilot-cli
+    # https://github.com/NixOS/nixpkgs/issues/507256
+    (import pkgs.npinsed.ai.nixpkgs-goose-cli {}).goose-cli
   ];
 }
