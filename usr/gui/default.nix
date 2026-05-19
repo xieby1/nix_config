6e48062091d5d64461adc67f52b37e943ed8ace6
xieby1 <xieby1@outlook.com>
2026-05-19 09:44:54 +0800

    feat(gui): add surfer

--- usr/gui/default.nix
+++ usr/gui/default.nix
@@ -69,6 +69,7 @@ config = lib.mkIf config.isGui {
     evince
     gnome-characters
     nautilus
+    pkgsu.surfer
   ] ++ pkgs.lib.optionals (builtins.currentSystem=="x86_64-linux") [
     imhex
     xelfviewer
