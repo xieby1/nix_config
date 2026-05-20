489eea8cdff38b4ad9fb73e2c4ea023714b8e108
xieby1 <xieby1@outlook.com>
2026-05-20 12:37:24 +0800

    bump: nixd

--- usr/cli/vim/nvim-lspconfig/nix/default.nix
+++ usr/cli/vim/nvim-lspconfig/nix/default.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: let
+  # TODO: use the nix from nixpkgs, maybe 26.05?
+  #       The current tilde support is added in 2026.05.20
   nixd = (pkgs.flake-compat {src = pkgs.npinsed.nixd;})
   .defaultNix.packages.${pkgs.stdenv.system}.default;
 in {
