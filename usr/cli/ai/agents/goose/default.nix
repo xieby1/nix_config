62fa46e0b58e0e69833b53590c2d439176ba5696
xieby1 <xieby1@outlook.com>
2026-05-25 08:41:08 +0800

    enhance(goose): add /model

--- usr/cli/ai/agents/goose/default.nix
+++ usr/cli/ai/agents/goose/default.nix
@@ -9,6 +9,12 @@
           url = "https://github.com/aaif-goose/goose/pull/9319.patch";
           sha256 = "0ca1bnn680br8i98c27h9w3ppyqqpscbxhm8mprxyd7chw40xj2a";
         })
+        # feat: add /model slash command to CLI for session model switching#8747
+        # Cons: only support change model, cannot change provider, cannot auto completion
+        (builtins.fetchurl {
+          url = "https://github.com/aaif-goose/goose/pull/8747.patch";
+          sha256 = "1d68gwhz2037abkri5kgl6a1fgafcribw7iz8r6ln6si02qqvlqi";
+        })
       ];
     };
   }).defaultNix.packages.${pkgs.stdenv.system}.default;
