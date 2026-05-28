c74720cf3dea757bae5519fd27406f66f01e5f3a
xieby1 <xieby1@outlook.com>
2026-05-28 11:07:16 +0800

    feat(ai: api): add bifrost

---
+++ usr/cli/ai/api/bifrost/package.nix
@@ -0,0 +1,19 @@
+# - Cons:
+#   - bifrost git repo is huge, the nix build needs to download hugh amount of npmDeps.
+#   - The simple nix bug fix is very slow: https://github.com/maximhq/bifrost/issues/2192
+let
+  pkgs = import <nixpkgs> {};
+  bifrost-pkgs = (pkgs.flake-compat { src = pkgs.npinsed.ai.bifrost; })
+                  .defaultNix.packages.${pkgs.stdenv.system};
+# See: https://github.com/maximhq/bifrost/issues/2192
+in (bifrost-pkgs.bifrost-http.override {
+  bifrost-ui = bifrost-pkgs.bifrost-ui.overrideAttrs (old: {
+    npmDeps = old.npmDeps.overrideAttrs (_: {
+      outputHash = "sha256-YniwFXRYyS8PpfabAAK0csyQLGrwUjONLTGXF7HINaI=";
+    });
+  });
+}).overrideAttrs (old: {
+  goModules = old.goModules.overrideAttrs (_: {
+    outputHash = "sha256-Sv3zT07ud32j6Yzc8SsjscBxYGu47hQoOOysQyiNB+I=";
+  });
+})
