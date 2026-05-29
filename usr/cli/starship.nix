7f1672746507c5006a7dd9e8f131374a67bc2258
xieby1 <xieby1@outlook.com>
2026-05-29 20:47:02 +0800

    feat(starship): support nix FHS

--- usr/cli/starship.nix
+++ usr/cli/starship.nix
@@ -7,6 +7,11 @@
         truncation_length = 0;
         truncate_to_repo = false;
       };
+      custom.fhs = {
+        when = "test -e /nix-support/is-fhsenv";
+        format = "[󱄅FHS]($style) ";
+        style = "bold yellow";
+      };
     };
   };
 }
