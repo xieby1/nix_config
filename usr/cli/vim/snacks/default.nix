90d695cd7777efcdfdc5ef41de942d648d189f4e
xieby1 <xieby1@outlook.com>
2026-05-31 13:42:03 +0800

    chore(vim: snacks): separate scroll

--- usr/cli/vim/snacks/default.nix
+++ usr/cli/vim/snacks/default.nix
@@ -9,17 +9,7 @@
       config = /*lua*/ ''
         require("snacks").setup({
           ${import ./image/lua.nix}
-          -- Comparisons:
-          -- - neoscroll.nvim: only support few scroll motion, does not support gg, G, S-up, S-down,
-          --                   see: https://github.com/karb94/neoscroll.nvim/issues/23
-          scroll = {
-            enabled = true,
-            filter = function(buf)
-              -- default filter does not allow in terminal, remove this limit!
-              -- return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and vim.bo[buf].buftype ~= "terminal"
-              return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false
-            end,
-          },
+          ${import ./scroll/lua.nix}
         })
       '';
     }];
