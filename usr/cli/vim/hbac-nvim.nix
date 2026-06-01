49dcd91341208f351f35229748270226481a0d77
xieby1 <xieby1@outlook.com>
2026-06-01 22:48:11 +0800

    chore(hbac-nvim): clean

--- usr/cli/vim/hbac-nvim.nix
+++ usr/cli/vim/hbac-nvim.nix
@@ -1,28 +1,23 @@
 #MC # hbac-nvim: auto close buffer
-{ config, pkgs, stdenv, lib, ... }:
-let
-  my-hbac = {
-    plugin = pkgs.vimUtils.buildVimPlugin {
-      name = "hbac.nvim";
-      src = pkgs.npinsed.nvim.hbac;
-      doCheck = false;
-    };
-    type = "lua";
-    config = /*lua*/ ''
-      require("hbac").setup({
-        autoclose     = true, -- set autoclose to false if you want to close manually
-        threshold     = 20, -- hbac will start closing unedited buffers once that number is reached
-        close_command = function(bufnr)
-          vim.api.nvim_buf_delete(bufnr, {})
-        end,
-        close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
-      })
-    '';
-  };
-in {
+{ pkgs, ... }: {
   programs.neovim = {
-    plugins = [
-      my-hbac
-    ];
+    plugins = [{
+      plugin = pkgs.vimUtils.buildVimPlugin {
+        name = "hbac.nvim";
+        src = pkgs.npinsed.nvim.hbac;
+        doCheck = false;
+      };
+      type = "lua";
+      config = /*lua*/ ''
+        require("hbac").setup({
+          autoclose     = true, -- set autoclose to false if you want to close manually
+          threshold     = 20, -- hbac will start closing unedited buffers once that number is reached
+          close_command = function(bufnr)
+            vim.api.nvim_buf_delete(bufnr, {})
+          end,
+          close_buffers_with_windows = false, -- hbac will close buffers with associated windows if this option is `true`
+        })
+      '';
+    }];
   };
 }
