e9949ded68b5bee82624235efc06084ea99a3857
xieby1 <xieby1@outlook.com>
2026-06-03 15:39:10 +0800

    chore(goose): use llm-agents

--- usr/cli/ai/agents/goose/package.nix
+++ usr/cli/ai/agents/goose/package.nix
@@ -1,12 +1,14 @@
 let
   pkgs = import <nixpkgs> {};
-in (pkgs.flake-compat {
-  src = pkgs.applyPatches {
-    name = "patched-goose-source";
-    src = pkgs.npinsed.ai.goose;
-    patches = [
-      # The upstream flake.nix is broken again! https://github.com/aaif-goose/goose/issues/9467
-      # It is funny, the flake.nix is broken again and again!
-    ];
-  };
-}).defaultNix.packages.${pkgs.stdenv.system}.default
+# in (pkgs.flake-compat {
+#   src = pkgs.applyPatches {
+#     name = "patched-goose-source";
+#     src = pkgs.npinsed.ai.goose;
+#     patches = [
+#       # The upstream flake.nix is broken again! https://github.com/aaif-goose/goose/issues/9467
+#       # It is funny that the flake.nix is broken again and again!
+#     ];
+#   };
+# }).defaultNix.packages.${pkgs.stdenv.system}.default
+  llm-agent-flake = pkgs.flake-compat {src = pkgs.npinsed.ai.llm-agents;};
+in llm-agent-flake.defaultNix.packages."${builtins.currentSystem}".goose-cli
