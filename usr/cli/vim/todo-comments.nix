a1c0a7ac1a2122d9e4e77b9962955ceb4beafe99
xieby1 <xieby1@outlook.com>
2026-06-03 09:59:02 +0800

    config(vim: todo-comments): add [t, ]t

--- usr/cli/vim/todo-comments.nix
+++ usr/cli/vim/todo-comments.nix
@@ -5,6 +5,14 @@
       type = "lua";
       config = /*lua*/''
         require("todo-comments").setup({})
+
+        vim.keymap.set("n", "]t", function()
+          require("todo-comments").jump_next()
+        end, { desc = "Next todo comment" })
+
+        vim.keymap.set("n", "[t", function()
+          require("todo-comments").jump_prev()
+        end, { desc = "Previous todo comment" })
       '';
     }];
   };
