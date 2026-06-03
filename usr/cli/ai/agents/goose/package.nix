7cb31ee69b24fe1471cf2c1a36689c56e5603cb4
xieby1 <xieby1@outlook.com>
2026-06-03 14:48:01 +0800

    bump: goose main

--- usr/cli/ai/agents/goose/package.nix
+++ usr/cli/ai/agents/goose/package.nix
@@ -5,17 +5,8 @@ in (pkgs.flake-compat {
     name = "patched-goose-source";
     src = pkgs.npinsed.ai.goose;
     patches = [
-      # fix(flake): #8514 - added output hashes to flake.nix ...#9319
-      (builtins.fetchurl {
-        url = "https://github.com/aaif-goose/goose/pull/9319.patch";
-        sha256 = "0ca1bnn680br8i98c27h9w3ppyqqpscbxhm8mprxyd7chw40xj2a";
-      })
-      # feat: add /model slash command to CLI for session model switching#8747
-      # Cons: only support change model, cannot change provider, cannot auto completion
-      (builtins.fetchurl {
-        url = "https://github.com/aaif-goose/goose/pull/8747.patch";
-        sha256 = "1d68gwhz2037abkri5kgl6a1fgafcribw7iz8r6ln6si02qqvlqi";
-      })
+      # The upstream flake.nix is broken again! https://github.com/aaif-goose/goose/issues/9467
+      # It is funny, the flake.nix is broken again and again!
     ];
   };
 }).defaultNix.packages.${pkgs.stdenv.system}.default
