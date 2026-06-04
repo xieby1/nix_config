1b76b8c3feb5f8ec1102ea1b802fc0e6f08741b9
xieby1 <xieby1@outlook.com>
2026-06-04 22:53:32 +0800

    chore(firefox: vimium): use toJSON

--- usr/gui/firefox/extensions/vimium.nix
+++ usr/gui/firefox/extensions/vimium.nix
@@ -12,13 +12,13 @@ in {
   home.file.vimium_sql = let
     # retrieve json:
     # sqlite3 storage-sync-v2.sqlite "SELECT data FROM storage_sync_data WHERE ext_id='{d7742d87-e61d-4b78-b8a1-b469842139fa}';"
-    json = /*json*/ ''{
-      "exclusionRules": [{
-        "passKeys": "",
-        "pattern": "*:77[0-9][0-9]*"
-      }],
-      "settingsVersion": "${lib.removePrefix "vimium-" vimium.name}"
-    }'';
+    json = lib.generators.toJSON {} {
+      exclusionRules = [{
+        passKeys = "";
+        pattern = "*:77[0-9][0-9]*";
+      }];
+      settingsVersion = lib.removePrefix "vimium-" vimium.name;
+    };
     relDir = "${config.programs.firefox.profilesPath}/xieby1";
     absDir = "${config.home.homeDirectory}/${relDir}";
     relTarget = "${relDir}/browser-extension-data/${vimium.addonId}/sql";
