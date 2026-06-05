6618e74f7eb86e6641a480928564a4b5b914ff23
xieby1 <xieby1@outlook.com>
2026-06-05 10:01:38 +0800

    config(todo-comments): minimal highlight

--- usr/cli/vim/todo-comments.nix
+++ usr/cli/vim/todo-comments.nix
@@ -4,7 +4,14 @@
       plugin = pkgs.vimPlugins.todo-comments-nvim;
       type = "lua";
       config = /*lua*/''
-        require("todo-comments").setup({})
+        require("todo-comments").setup({
+          highlight = {
+            multiline = false,
+            before = "",
+            after = "",
+            comments_only = false,
+          },
+        })
 
         vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
         vim.keymap.set("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" })
