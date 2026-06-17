385a33c4439b3a9051b352419c387fd0c04e0e9b
xieby1 <xieby1@outlook.com>
2026-06-17 18:39:11 +0800

    feat(avante-nvim): use acp

--- usr/cli/vim/avante-nvim/default.nix
+++ usr/cli/vim/avante-nvim/default.nix
@@ -21,21 +21,11 @@
         -- this file can contain specific instructions for your project
         -- instructions_file = "avante.md",
         -- for example
-        provider = "minimax",
-        providers = {
-          deepseek = {
-            __inherited_from = "openai",
-            -- api_key_name = "DEEPSEEK_API_KEY",
-            parse_api_key = function() return "${config.ai.deepseek.api_key}" end,
-            endpoint = "${config.ai.deepseek.api_endpoint}",
-            model = "deepseek-reasoner",
-          },
-          minimax = {
-            __inherited_from = "claude",
-            -- api_key_name = "DEEPSEEK_API_KEY",
-            parse_api_key = function() return "${config.ai.minimax-china.api_key}" end,
-            endpoint = "${config.ai.minimax-china.api_endpoint}",
-            model = "MiniMax-M2.7",
+        provider = "goose-${config.ai.jw-codex.id}",
+        acp_providers = {
+          ["goose-${config.ai.jw-codex.id}"] = {
+            command = "goose-${config.ai.jw-codex.id}",
+            args = {"acp"},
           },
         },
         windows = {
