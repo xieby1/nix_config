cb8be2edc315462145ea62999e5a4ec925997c53
xieby1 <xieby1@outlook.com>
2026-06-18 13:40:03 +0800

    config(vim): lualine

--- usr/cli/vim/lualine.nix
+++ usr/cli/vim/lualine.nix
@@ -5,8 +5,33 @@
       type = "lua";
       config = /*lua*/ ''
         require("lualine").setup({
+          options = {
+            -- make the inactive theme same as active theme
+            theme = (function()
+              local t = require("lualine.themes.auto")
+              t.inactive = {
+                a = t.normal.a,
+                b = t.normal.b,
+                c = t.normal.c,
+              }
+              return t
+            end)(),
+          },
           sections = {
-            lualine_x = {'filetype'},
+            lualine_a = {'filename'},
+            lualine_b = {'branch', 'diagnostics'},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {'filetype'},
+            lualine_z = {'location', 'progress'}
+          },
+          inactive_sections = {
+            lualine_a = {'filename'},
+            lualine_b = {'branch', 'diagnostics'},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {'filetype'},
+            lualine_z = {'location', 'progress'}
           },
         })
       '';
