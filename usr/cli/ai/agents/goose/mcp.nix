009dee2b52c9ca74e5012f1b50210e4d643a54af
xieby1 <xieby1@outlook.com>
2026-06-19 22:27:14 +0800

    feat(goose mcp): add basic-memory

--- usr/cli/ai/agents/goose/mcp.nix
+++ usr/cli/ai/agents/goose/mcp.nix
@@ -26,6 +26,20 @@
           args = ["stdio"];
           envs = { GITHUB_PERSONAL_ACCESS_TOKEN = lib.trim (builtins.readFile ~/Gist/Vault/AI/github-mcp-server-all-repos-daily.txt);};
         };
+        # - There are not nixpkgs/basic-memory/3rd-party nix package for basic-memory.
+        # - No issues/discussions mention nix package.
+        # - Using AI to package basic-memory does not works well.
+        # So I use uvx here.
+        basic-memory = {
+          enabled = true;
+          type = "stdio";
+          name = "basic-memory";
+          cmd = "${pkgs.uv}/bin/uvx";
+          args = ["basic-memory" "mcp"];
+          envs = {
+            LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ];
+          };
+        };
       };
     };
   };
