a9a6263b46dd6557badb462deed3eecb46f90e75
xieby1 <xieby1@outlook.com>
2026-06-22 23:24:57 +0800

    chore(pi): pi-hermes-memory

--- usr/cli/ai/agents/pi/extensions/pi-hermes-memory.nix
+++ usr/cli/ai/agents/pi/extensions/pi-hermes-memory.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }: let
-  buildNpmPackage = pkgs.buildNpmPackage.override {
-    nodejs = pkgs.nodejs_24;
-  };
+  # The node-gyp should be backed by the same version of nodejs as pi,
+  # thus we use pkgspi.
+  pkgspi = import (
+    pkgs.flake-compat {src = pkgs.npinsed.ai.llm-agents;}
+  ).outputs.inputs.nixpkgs {};
 in {
   home.file.".pi/agent/extensions/pi-hermes-memory" = {
-    source = buildNpmPackage {
+    source = pkgspi.buildNpmPackage {
       name = "pi-hermes-memory";
       src = pkgs.npinsed.ai.pi.pi-hermes-memory;
       npmDepsHash = "sha256-t5jQWrucm0Fm5V2PkC0btVWe7cwvrp1HuKqTwl38xA8=";
       dontNpmBuild = true;
-      nativeBuildInputs = with pkgs; [
-        nodejs_24.pkgs.node-gyp
+      nativeBuildInputs = with pkgspi; [
+        node-gyp
         python3
       ];
     } + /lib/node_modules/pi-hermes-memory;
