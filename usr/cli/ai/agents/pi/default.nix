afd2287c4a91f89c07206e82569c36ba989e4f77
xieby1 <xieby1@outlook.com>
2026-06-23 10:36:28 +0800

    config(pi): sync pi-hermes-memory files

--- usr/cli/ai/agents/pi/default.nix
+++ usr/cli/ai/agents/pi/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   imports = [
     ./extensions
     ./providers
@@ -7,4 +7,8 @@
     (pkgs.flake-compat {src = pkgs.npinsed.ai.llm-agents;})
       .defaultNix.packages.x86_64-linux.pi
   ];
+  home.file.".pi/agent/projects-memory".source = config.lib.file.mkOutOfStoreSymlink ~/Gist/Data/pi/projects-memory;
+  home.file.".pi/agent/pi-hermes-memory/MEMORY.md".source = config.lib.file.mkOutOfStoreSymlink ~/Gist/Data/pi/pi-hermes-memory/MEMORY.md;
+  home.file.".pi/agent/pi-hermes-memory/USER.md".source = config.lib.file.mkOutOfStoreSymlink ~/Gist/Data/pi/pi-hermes-memory/USER.md;
+  home.file.".pi/agent/pi-hermes-memory/skills".source = config.lib.file.mkOutOfStoreSymlink ~/Gist/Data/pi/pi-hermes-memory/skills;
 }
