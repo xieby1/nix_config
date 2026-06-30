ca84c7db26ce7d8403253f17d34476a5b41235d9
xieby1 <xieby1@outlook.com>
2026-06-30 22:29:03 +0800

    config(kitty): font size

--- usr/gui/kitty/default.nix
+++ usr/gui/kitty/default.nix
@@ -66,7 +66,9 @@
     };
     font = {
       name = "";
-      size = 12;
+      # My zsh does not export HOSTNAME, so `builtins.getEnv "HOSTNAME"` does not work.
+      # Therefore, I use /etc/hostname here.
+      size = (import ~/Gist/Config/kitty-font-size.nix).${builtins.readFile /etc/hostname} or 14;
     };
     settings = {
       allow_remote_control = "yes";
