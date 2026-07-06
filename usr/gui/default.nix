9dc1ed2e71ed05db87e58c933a593b6c8a25e1e4
xieby1 <xieby1@outlook.com>
2026-07-06 20:59:18 +0800

    fix(gui): use loupe for images

--- usr/gui/default.nix
+++ usr/gui/default.nix
@@ -33,6 +33,13 @@ config = lib.mkIf config.isGui {
     enable = true;
     defaultApplications = {
       "application/pdf" = [ "org.gnome.Evince.desktop" ];
+      "image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+      "image/png" = [ "org.gnome.Loupe.desktop" ];
+      "image/gif" = [ "org.gnome.Loupe.desktop" ];
+      "image/webp" = [ "org.gnome.Loupe.desktop" ];
+      "image/tiff" = [ "org.gnome.Loupe.desktop" ];
+      "image/bmp" = [ "org.gnome.Loupe.desktop" ];
+      "image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
     };
   };
 
