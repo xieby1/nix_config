9cc49f5c435d63815cc7412b704f7ef1a89bb34b
xieby1 <xieby1@outlook.com>
2026-07-06 18:59:13 +0800

    config(pi): more retries

--- usr/cli/ai/agents/pi/default.nix
+++ usr/cli/ai/agents/pi/default.nix
@@ -8,4 +8,10 @@
     (pkgs.flake-compat {src = pkgs.npinsed.ai.pi.llm-agents;})
       .defaultNix.packages.x86_64-linux.pi
   ];
+
+  yq-merge.".pi/agent/settings.json".expr.retry = {
+    enabled = true;
+    maxRetries = 6;
+    baseDelayMs = 1000;
+  };
 }
