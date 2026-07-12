f08850cf8a9c8815005c1c277860c120a968330c
xieby1 <xieby1@outlook.com>
2026-07-12 16:42:20 +0800

    feat(clash tui): add config

--- usr/cli/clash/tui/default.nix
+++ usr/cli/clash/tui/default.nix
@@ -8,4 +8,14 @@ in {
   cachix_packages = [
     mihomo-tui
   ];
+  yq-merge.".config/mihomo-tui/config.yaml" = {
+    generator = builtins.toJSON;
+    expr = let
+      clash-config-base = builtins.fromJSON (builtins.readFile ~/Gist/clash/base.json);
+    in {
+      mihomo-api = "http://${clash-config-base.external-controller}";
+      mihomo-secret = clash-config-base.secret;
+      log-file = "/tmp/mihomo-tui.log";
+    };
+  };
 }
