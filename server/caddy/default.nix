dcdfad2ae62a8145c757e8357107726fd5f2fbef
xieby1 <xieby1@outlook.com>
2026-07-12 23:49:06 +0800

    Generate Caddyfile with Nix

--- server/caddy/default.nix
+++ server/caddy/default.nix
@@ -1,8 +1,32 @@
 { config, pkgs, lib, ... }:
 let
   cfg = config.my.server.caddyAuthelia;
-  repoDir = "${config.home.homeDirectory}/.config/nixpkgs";
-  caddyfile = "${repoDir}/server/caddy/Caddyfile";
+  caddyfile = pkgs.writeText "Caddyfile" ''
+    {
+      auto_https disable_redirects
+      skip_install_trust
+    }
+
+    https://127.0.0.1:8443 {
+      tls internal
+
+      redir /app /app/
+
+      handle /app/* {
+        route {
+          forward_auth 127.0.0.1:9091 {
+            uri /api/authz/forward-auth
+            copy_headers Remote-User Remote-Groups Remote-Name Remote-Email
+          }
+
+          uri strip_prefix /app
+          reverse_proxy 127.0.0.1:3000
+        }
+      }
+
+      reverse_proxy 127.0.0.1:9091
+    }
+  '';
 in {
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.caddy ];
