1cc8553a00c6e983901740645d2da6d8216d52e0
xieby1 <xieby1@outlook.com>
2026-07-14 22:32:10 +0800

    feat(server): add cli essentials

--- server/default.nix
+++ server/default.nix
@@ -4,11 +4,19 @@
     ./authelia
     ./caddy
 
+    # TODO: group ../usr/*
+    ../usr/modules
+
     # TODO group following to ../usr/cli/sh
     ../usr/cli/starship.nix
     ../usr/cli/zsh
 
     ../usr/cli/git.nix
+
+    ../usr/cli/clash
+
+    # TODO: clean!
+    ../usr/cli/vim
   ];
 
   options.my.server.caddyAuthelia.enable = lib.mkEnableOption "Caddy + Authelia prototype user services";
