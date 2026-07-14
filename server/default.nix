a0a880e8de5cc38ac3e18ca3f899469d15a45536
xieby1 <xieby1@outlook.com>
2026-07-14 21:33:16 +0800

    feat(server): add shell essentials

--- server/default.nix
+++ server/default.nix
@@ -3,9 +3,25 @@
   imports = [
     ./authelia
     ./caddy
+
+    # TODO group following to ../usr/cli/sh
+    ../usr/cli/starship.nix
+    ../usr/cli/zsh
+
+    ../usr/cli/git.nix
   ];
 
   options.my.server.caddyAuthelia.enable = lib.mkEnableOption "Caddy + Authelia prototype user services";
 
-  config.my.server.caddyAuthelia.enable = true;
+  config = {
+    targets.genericLinux.enable = true;
+    targets.genericLinux.gpu.enable = false;
+
+    my.server.caddyAuthelia.enable = true;
+
+    # TODO: deduplcate, as this snippet is also in user/default.nix
+    programs.zsh.initContent = ''
+      . ${toString ../scripts/bootstrap/main.sh}
+    '';
+  };
 }
