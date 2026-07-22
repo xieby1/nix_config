a81edcb3de00b103da3302e892f9e17cd865b229
xieby1 <xieby1@outlook.com>
2026-07-22 11:58:00 +0800

    Document root lingering for server services

--- server/default.nix
+++ server/default.nix
@@ -15,6 +15,13 @@
     targets.genericLinux.enable = true;
     targets.genericLinux.gpu.enable = false;
 
+    # NOTE: These server daemons run as root Home Manager user services. The host
+    # must keep root's systemd user manager alive after logout:
+    #
+    #   loginctl enable-linger root
+    #
+    # Without lingering, the last root SSH logout stops user@0.service, which
+    # stops Caddy/Authelia and browsers may report PR_END_OF_FILE_ERROR.
     my.server.caddyAuthelia.enable = true;
 
     # TODO: deduplcate, as this snippet is also in user/default.nix
