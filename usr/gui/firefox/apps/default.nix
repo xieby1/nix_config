17ed49f6f994e54a36e15dc9037a40eb3fbf52a1
xieby1 <xieby1@outlook.com>
2026-07-23 23:35:30 +0800

    fix(firefox apps): sixu

--- usr/gui/firefox/apps/default.nix
+++ usr/gui/firefox/apps/default.nix
@@ -137,7 +137,7 @@
     };
   }{
     name = "sixu";
-    url = "http://xieby1.cn/sixu/xby/";
+    url = "https://xieby1.cn/sixu/xby/";
     icon = pkgs.npinsed.my.sixu + /src/app/ui/web/frontend/public/sixu-icon.svg;
   }];
 }
