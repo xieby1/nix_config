7e4a980d386b41e58ec17ce566b6778e2052a3e1
xieby1 <xieby1@outlook.com>
2026-07-30 22:31:38 +0800

    chore(dingtalk): cachix

--- usr/gui/dingtalk.nix
+++ usr/gui/dingtalk.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }: {
   home.packages = [
-    # TODO: cachix openssl (3min+ build time) and mesa (3min+ build time), see ~/.local/state/nix-output-monitor/build-reports.csv
     # As there is not dingtalk in nixpkgs, so we use dingtalk in nur.
     pkgs.nur.repos.yakkhini.dingtalk
   ];
+  # Cachix long-build-time packages, see ~/.local/state/nix-output-monitor/build-reports.csv
+  cachix_packages = (
+    # openssl: 3min+ build time)
+    builtins.filter
+      (p: pkgs.lib.hasInfix "openssl" p)
+      pkgs.nur.repos.yakkhini.dingtalk.buildInputs
+  );
 }
