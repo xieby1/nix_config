1a9d38fd6b583944b4472baf4fbef032a30a20f8
xieby1 <xieby1@outlook.com>
2026-09-08 11:20:50 +0800

    chore(nvim): remove trim

--- usr/cli/basic/nvim/lz-n/default.nix
+++ usr/cli/basic/nvim/lz-n/default.nix
@@ -1,13 +1,12 @@
 # # lz.n vs lazy.nvim:
 # - lazy.nvim contains too many unnecessary features (unrelated to lazy)!
-{ pkgs, lib, ... }: {
+{ pkgs, ... }: {
   programs.neovim.plugins = [
     pkgs.vimPlugins.lz-n
   ];
 
   imports = [
     ./module
-    ./trim.nix
   ];
   my.neovim.lz-n = [];
 }
