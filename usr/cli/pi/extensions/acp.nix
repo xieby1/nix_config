cd25930f4fd3d2cef7ed7a67011d4851f793bb30
xieby1 <xieby1@outlook.com>
2026-09-21 17:20:19 +0800

    chore(pi extensions): bump pi-acp to v0.0.33

--- usr/cli/pi/extensions/acp.nix
+++ usr/cli/pi/extensions/acp.nix
@@ -3,7 +3,7 @@
     pkgs.buildNpmPackage (finalAttrs: {
       name = "pi-acp";
       src = pkgs.npinsed.ai.pi.acp;
-      npmDepsHash = "sha256-qN+b/tMbnJLkWjotl3XrA0nfZ3KT/mT6gM+n3Qiz8Wk=";
+      npmDepsHash = "sha256-/fX79XucKojL/6gZbK5eizEfrXso8rlTgiHfJffmDuY=";
       npmRebuildFlags = [ "--ignore-scripts" ];
     })
   )];
