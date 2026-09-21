778906722b85892f84563a8eb5d348e9fd6a8ce4
xieby1 <xieby1@outlook.com>
2026-09-21 20:18:56 +0800

    chore(nvim): use nixpkgs kitty-scrollback.nvim, drop npins pin
    nixpkgs now carries v9.2.0, newer than the npins-pinned v8.0.0. The
    existing postPatch still applies.

--- usr/gui/kitty/kitty-scrollback-nvim.nix
+++ usr/gui/kitty/kitty-scrollback-nvim.nix
@@ -3,8 +3,7 @@
     plugins = [{
       plugin = pkgs.vimUtils.buildVimPlugin {
         name = "kitty-scrollback.nvim";
-        # TODO: use the kitty-scrollback.nvim from nixpkgs when it reachs 8.0.0
-        src = pkgs.npinsed.nvim.kitty-scrollback-nvim;
+        src = pkgs.vimPlugins.kitty-scrollback-nvim;
         postPatch = ''
           # Move status icons from top-right to bottom-left.
           substituteInPlace lua/kitty-scrollback/windows.lua \
@@ -28,7 +27,7 @@
   };
   programs.kitty.extraConfig = ''
     # kitty-scrollback.nvim Kitten alias
-    action_alias kitty_scrollback_nvim kitten '${pkgs.npinsed.nvim.kitty-scrollback-nvim}/python/kitty_scrollback_nvim.py'
+    action_alias kitty_scrollback_nvim kitten '${pkgs.vimPlugins.kitty-scrollback-nvim}/python/kitty_scrollback_nvim.py'
     # Browse scrollback buffer in nvim
     map kitty_mod+h kitty_scrollback_nvim
     # Browse output of the last shell command in nvim
