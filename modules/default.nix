98894911c06dd0ba51c93b86b8bac78f122ea14b
xieby1 <xieby1@outlook.com>
2026-09-24 08:51:00 +0800

    chore: no more isCli

--- modules/default.nix
+++ modules/default.nix
@@ -14,15 +14,6 @@
       default = 8889;
       description = "代理端口号，诸多网络程序需要用，比如clash和tailscale";
     };
-    isCli = lib.mkOption {
-      type = lib.types.bool;
-      default = (builtins.getEnv "DISPLAY")=="";
-      description = ''
-        `isCli`和`isGui`的默认值通过环境变量`DISPLAY`来判断是否是CLI或GUI环境。
-        这个方法有**局限**，比如ssh连接到一台有GUI的电脑上，ssh里是没有设置环境变量`DISPLAY`的。
-        因此更好的方法是显示地声明sCli和isGui的值。
-      '';
-    };
     isGui = lib.mkOption {
       type = lib.types.bool;
       default = (builtins.getEnv "DISPLAY")!="";
