7714c4cfd57fa89be5298dcc478e4ef5d8093c98
xieby1 <xieby1@outlook.com>
2026-09-24 08:36:43 +0800

    chore: move usr common config to usr/cli/basic/

--- server/default.nix
+++ server/default.nix
@@ -25,18 +25,6 @@
     # stops Caddy/Authelia and browsers may report PR_END_OF_FILE_ERROR.
     my.server.caddyAuthelia.enable = true;
 
-    # TODO: deduplcate, as this snippet is also in user/default.nix
-    programs.zsh.initContent = ''
-      . ${toString ../scripts/bootstrap/main.sh}
-    '';
-    # envExtra gives non-interactive zsh, such as ssh 'cmd', the existing pin paths.
-    # initContent runs the heavier bootstrap repair only for interactive shells.
-    programs.zsh.envExtra = /*bash*/ ''
-      if [[ -e "$HOME/.config/npins/nixpkgs" && -e "$HOME/.config/npins/home-manager" ]]; then
-        export NIX_PATH="nixpkgs=$HOME/.config/npins/nixpkgs:home-manager=$HOME/.config/npins/home-manager:nixos-config=/etc/nixos/configuration.nix"
-      fi
-    '';
-
     # TODO: deduplcate
     services.syncthing.enable = true;
 
